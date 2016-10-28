e4e75b60da7258c72bee189bce21458272c212c7
nicholejeannine <nklerk@alopias.com>
2016-10-28 13:07:16 -0700

    Configure scaffold generators to not produce annoying files

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,19 @@ module Railstest
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    config.generators do |g|
+      g.helper = false
+      g.assets = false
+      g.jbuilder = false
+      g.test_framework :rspec,
+                       fixtures: false,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: true,
+                       controller_specs: true,
+                       request_specs: false
+
+    end
   end
 end
