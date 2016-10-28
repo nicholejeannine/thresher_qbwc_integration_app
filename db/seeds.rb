952fbb00f8ea946b4e0742c4e46776b1a9a3ddca
nicholejeannine <nklerk@alopias.com>
2016-10-28 14:18:25 -0700

    Seed customer job

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+QBWC.clear_jobs
+QBWC.add_job(:list_customers, true, '', ListCustomerWorker)
