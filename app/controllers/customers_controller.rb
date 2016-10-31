19f4dee066cbca90ba7132686db71b378b33c194
nklerk <nklerk@alopias.com>
2016-10-31 08:32:36 -0700

    Remove controller actions whose corresponding routes have been deleted

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,5 +1,5 @@
 class CustomersController < ApplicationController
-  before_action :set_customer, only: [:show, :edit, :update, :destroy]
+  before_action :set_customer, only: [:show]
 
   # GET /customers
   # GET /customers.json
@@ -10,64 +10,6 @@ class CustomersController < ApplicationController
   # GET /customers/1
   # GET /customers/1.json
   def show
-  end
-
-  # GET /customers/new
-  def new
-    @customer = Customer.new
-  end
-
-  # GET /customers/1/edit
-  def edit
-  end
-
-  # POST /customers
-  # POST /customers.json
-  def create
-    @customer = Customer.new(customer_params)
-
-    respond_to do |format|
-      if @customer.save
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
-        format.json { render :show, status: :created, location: @customer }
-        format.xml { redirect_to @customer, notice: 'Customer was successfully created.' }
-      else
-        format.html { render :new }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-        format.xml { render :new }
-
-      end
-    end
-  end
-
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
-  def update
-    respond_to do |format|
-      if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @customer }
-        format.xml { redirect_to @customer, notice: 'Customer was successfully updated.' }
-
-      else
-        format.html { render :edit }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-        format.xml { render :edit }
-
-      end
-    end
-  end
-
-  # DELETE /customers/1
-  # DELETE /customers/1.json
-  def destroy
-    @customer.destroy
-    respond_to do |format|
-      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
-      format.json { head :no_content }
-      format.xml { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
-
-    end
   end
 
   private
