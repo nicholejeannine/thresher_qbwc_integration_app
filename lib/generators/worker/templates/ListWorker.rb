48a267abb494b391266d9e5587c191f70c3d418a
nklerk <nklerk@alopias.com>
2016-10-31 08:36:43 -0700

    Modify generator template for list workers

--- lib/generators/worker/templates/ListWorker.rb
+++ lib/generators/worker/templates/ListWorker.rb
@@ -13,11 +13,20 @@ class <%= name.titleize %>ListWorker < QBWC::Worker
   def handle_response(response, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
-    #return if complete
+    columns = <%=name.classify%>.column_names
     response['<%=name.underscore%>_ret'].each do |qb|
-      @<%=name.underscore%> = <%=name.camelize%>.find_or_create_by(:list_id => qb['list_id'], qb.from_qbxml)
-      Rails.logger.info("<%=name.camelize%>: #{qb}")
-      Rails.logger.error(@<%=name.underscore%>.errors) if @<%=name.underscore%>.errors?
+    <%=name.underscore%> = <%=name.classify%>.find_or_initialize_by(:id => qb['list_id'])
+
+      hash = qb.to_hash
+       hash.each do |key, value|
+       if columns.include?(key.to_s)
+          <%=name.underscore%>.send("#{key}=", value)
+        elsif key.match /ref_$/
+         <%=name.underscore%>.send("#{key.sub('ref_', '')}=", value['list_id'])
+       end
     end
+      <%=name.underscore%>.save
+     end
   end
+
 end
