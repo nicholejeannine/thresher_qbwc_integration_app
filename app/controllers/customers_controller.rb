554e59141e3cbc819de01aae97f41b8fc4974d1d
nicholejeannine <nklerk@alopias.com>
2016-11-04 09:13:36 -0700

    Slimmer controllers

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,54 +12,7 @@ class CustomersController < ApplicationController
   def show
   end
 
-  # GET /customers/new
-  def new
-    @customer = Customer.new
-  end
-
-  # GET /customers/1/edit
-  def edit
-  end
-
-  # POST /customers
-  # POST /customers.json
-  def create
-    @customer = Customer.new(customer_params)
-
-    respond_to do |format|
-      if @customer.save
-        format.html { redirect_to @customer, notice: 'Customer was successfully created.' }
-        format.json { render :show, status: :created, location: @customer }
-      else
-        format.html { render :new }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /customers/1
-  # PATCH/PUT /customers/1.json
-  def update
-    respond_to do |format|
-      if @customer.update(customer_params)
-        format.html { redirect_to @customer, notice: 'Customer was successfully updated.' }
-        format.json { render :show, status: :ok, location: @customer }
-      else
-        format.html { render :edit }
-        format.json { render json: @customer.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /customers/1
-  # DELETE /customers/1.json
-  def destroy
-    @customer.destroy
-    respond_to do |format|
-      format.html { redirect_to customers_url, notice: 'Customer was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
+  
 
   private
     # Use callbacks to share common setup or constraints between actions.
--- app/controllers/estimates_controller.rb
+++ app/controllers/estimates_controller.rb
@@ -12,54 +12,6 @@ class EstimatesController < ApplicationController
   def show
   end
 
-  # GET /estimates/new
-  def new
-    @estimate = Estimate.new
-  end
-
-  # GET /estimates/1/edit
-  def edit
-  end
-
-  # POST /estimates
-  # POST /estimates.json
-  def create
-    @estimate = Estimate.new(estimate_params)
-
-    respond_to do |format|
-      if @estimate.save
-        format.html { redirect_to @estimate, notice: 'Estimate was successfully created.' }
-        format.json { render :show, status: :created, location: @estimate }
-      else
-        format.html { render :new }
-        format.json { render json: @estimate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /estimates/1
-  # PATCH/PUT /estimates/1.json
-  def update
-    respond_to do |format|
-      if @estimate.update(estimate_params)
-        format.html { redirect_to @estimate, notice: 'Estimate was successfully updated.' }
-        format.json { render :show, status: :ok, location: @estimate }
-      else
-        format.html { render :edit }
-        format.json { render json: @estimate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /estimates/1
-  # DELETE /estimates/1.json
-  def destroy
-    @estimate.destroy
-    respond_to do |format|
-      format.html { redirect_to estimates_url, notice: 'Estimate was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
