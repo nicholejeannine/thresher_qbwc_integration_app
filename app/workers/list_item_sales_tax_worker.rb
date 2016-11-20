d5c373bc6188da514ef31c0acb54db1343fb2ada
nklerk <nklerk@alopias.com>
2016-11-20 12:03:33 -0800

    test

--- app/workers/list_item_sales_tax_worker.rb
+++ app/workers/list_item_sales_tax_worker.rb
@@ -3,35 +3,29 @@ class ListItemSalesTaxWorker < QBWC::Worker
   def requests(job, session, data)
     {
       :item_sales_tax_query_rq => {
-        :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
-        :max_returned => 100
+        :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" }
       }
     }
   end
 
   def handle_response(response, session, job, request, data)
     # handle_response will get item_sales_taxs in groups of 100. When this is 0, we're done.
-    complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
+    # complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
     columns = ItemSalesTax.column_names
     response['item_sales_tax_ret'].each do |qb|
       item_sales_tax = ItemSalesTax.find_or_initialize_by(:id => qb['list_id'])
       qb.to_hash.each do |key, value|
-      if key.match /xml_attributes/
-        next
-      elsif columns.include?(key.to_s)
+      if columns.include?(key.to_s)
             item_sales_tax.send("#{key}=", value)
        elsif value.class == Qbxml::Hash
           value.each  do |k, v|
-            if k == 'list_id' || k == 'owner_id'
-              name = key.sub(/ref$/, "id")
-              name.sub!(/ret$/, "id")
+            if k.match /id$/
+              name = key.sub(/ref$/, "id").sub(/ret$/, "id")
               item_sales_tax.send("#{name}=", v)
-            else
-              next
             end  # end if k == 'list_id' || k == 'owner_id'
           end # end value.each do |k,v|
-        end # end for each |key, value|
-      end # end parse
+        end # end if value is hash
+      end # end each
       if item_sales_tax.save
         Rails.logger.info("great success")
       else
