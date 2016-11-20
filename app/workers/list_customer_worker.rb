3761c6f7b24dac48599a9740d286f07442b4195a
nklerk <nklerk@alopias.com>
2016-11-20 10:32:05 -0800

    Test

--- app/workers/list_customer_worker.rb
+++ app/workers/list_customer_worker.rb
@@ -4,10 +4,10 @@ class ListCustomerWorker < QBWC::Worker
 
   def requests(job, session, data)
     {
-        :customer_query_rq => {
-            :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
-            :max_returned => 100
-        }
+      :customer_query_rq => {
+        :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
+        :max_returned => 100
+      }
     }
   end
 
@@ -18,43 +18,35 @@ class ListCustomerWorker < QBWC::Worker
     response['customer_ret'].each do |qb|
       customer = Customer.find_or_initialize_by(:id => qb['list_id'])
       qb.to_hash.each do |key, value|
-        if value.class == Qbxml::Hash
+        if key.match /block$|xml_attributes/
+            next
+        elsif key.match /ship_address$|bill_address$/
+          customer.send("#{key}_addr1=", value['addr1'])
+          customer.send("#{key}_addr2=", value['addr2'])
+          customer.send("#{key}_addr3=", value['addr3'])
+          customer.send("#{key}_addr4=", value['addr4'])
+          customer.send("#{key}_addr5=", value['addr5'])
+          customer.send("#{key}_city=", value['city'])
+          customer.send("#{key}_state=", value['state'])
+          customer.send("#{key}_postal_code=", value['postal_code'])
+          customer.send("#{key}_note=", value['note'])
+        elsif value.class == Qbxml::Hash
           value.each  do |k, v|
             if k == 'list_id'
               name = key.sub(/ref$/, "id")
               name.sub!(/ret$/, "id")
-              Customer.send("#{name}=", v)
-            end
-          end
-       end
-    #   if key.match /block$|xml_attributes/
-    #     next
-    #   elsif key.match /ship_address$|bill_address$/
-    #     customer.send("#{key}_addr1=", value['addr1'])
-    #     customer.send("#{key}_addr2=", value['addr2'])
-    #     customer.send("#{key}_addr3=", value['addr3'])
-    #     customer.send("#{key}_addr4=", value['addr4'])
-    #     customer.send("#{key}_addr5=", value['addr5'])
-    #     customer.send("#{key}_city=", value['city'])
-    #     customer.send("#{key}_state=", value['state'])
-    #     customer.send("#{key}_postal_code=", value['postal_code'])
-    #     customer.send("#{key}_note=", value['note'])
-    #   elsif value.class == Qbxml::Hash
-    #     name = key.remove(/ref$|ret$/)
-    #     customer.send("#{name}_id=", value['list_id'])
-    #     Rails.logger.warn("Customer.send #{name}_id=#{value['list_id']}")
-    #   elsif columns.include?(key.to_s)
-    #     customer.send("#{key}=", value)
-    #      Rails.logger.warn("Customer.send #{key}=#{value}")
-    #   else
-    #     Rails.logger.warn("NOT SENT: #{key}:#{value}")
-    #   end  # end conditional
-    end # end for each pair
+              customer.send("#{name}=", v)
+            end  # end if k == 'list_id'
+          end # end value.each do |k,v|
+        elsif columns.include?(key.to_s)
+            customer.send("#{key}=", value)
+        end # end if parsing ridiculousness
+      end # end for each |key, value|
       if customer.save
-          Rails.logger.info("great success")
+        Rails.logger.info("great success")
       else
-         Rails.logger.info("Not saved:  #{customer.errors}")
-     end
-  end
- end
-end
+        Rails.logger.info("Not saved:  #{customer.errors}")
+      end # end if customer save
+    end # end for each customer
+  end # end handle response
+end # end worker class
