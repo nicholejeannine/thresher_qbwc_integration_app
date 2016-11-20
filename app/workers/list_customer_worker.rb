00e9adfffb0e70ea547afbecd56590cdbf05a0e7
nklerk <nklerk@alopias.com>
2016-11-20 02:51:04 -0800

    Test

--- app/workers/list_customer_worker.rb
+++ app/workers/list_customer_worker.rb
@@ -18,7 +18,7 @@ class ListCustomerWorker < QBWC::Worker
     response['customer_ret'].each do |qb|
     customer = Customer.find_or_initialize_by(:id => qb['list_id'])
     qb.to_hash.each do |key, value|
-      if key.match /ship_address$ | bill_address$/
+      if key == 'ship_address' || key == 'bill_address'
         customer.send("#{key}_addr1=", value['addr1'])
         customer.send("#{key}_addr2=", value['addr2'])
         customer.send("#{key}_addr3=", value['addr3'])
