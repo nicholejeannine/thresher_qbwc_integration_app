636ba17b985956711d82c46a291546dcb0eef1fc
nklerk <nklerk@alopias.com>
2016-11-20 10:42:02 -0800

    Revise generated worker and test with estimate

--- app/workers/list_estimate_worker.rb
+++ app/workers/list_estimate_worker.rb
@@ -9,35 +9,43 @@ class ListEstimateWorker < QBWC::Worker
     }
   end
 
-  # There's no way this will work yet but .... getting there hopefully
   def handle_response(response, session, job, request, data)
-    # handle_response will get customers in groups of 100. When this is 0, we're done.
+    # handle_response will get estimates in groups of 100. When this is 0, we're done.
     complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
     columns = Estimate.column_names
     response['estimate_ret'].each do |qb|
-    estimate = Estimate.find_or_initialize_by(:id => qb['txn_id'])
+      id = qb['list_id'] || qb['txn_id']
+      estimate = Estimate.find_or_initialize_by(:id => id)
       qb.to_hash.each do |key, value|
-       if columns.include?(key.to_s)
-          estimate.send("#{key}=", value)
-        elsif key.match /ref$/
-         estimate.send("#{key.sub('ref', 'id')}=", value['list_id'])
-       elsif key.match /ship_address$ | bill_address$/
-         estimate.send("#{key}_addr1=", value['addr1'])
-         estimate.send("#{key}_addr2=", value['addr2'])
-         estimate.send("#{key}_city=", value['city'])
-         estimate.send("#{key}_state=", value['state'])
-         estimate.send("#{key}_postal_code=", value['postal_code'])
-         estimate.send("#{key}_note=", value['note'])
-       else
-         Rails.logger.info("ERROR SENDING #{key}: #{value}")
-        end  # end conditional
-       end # end for each pair
+        if key.match /block$|xml_attributes/
+            next
+        elsif key.match /ship_address$|bill_address$/
+          estimate.send("#{key}_addr1=", value['addr1'])
+          estimate.send("#{key}_addr2=", value['addr2'])
+          estimate.send("#{key}_addr3=", value['addr3'])
+          estimate.send("#{key}_addr4=", value['addr4'])
+          estimate.send("#{key}_addr5=", value['addr5'])
+          estimate.send("#{key}_city=", value['city'])
+          estimate.send("#{key}_state=", value['state'])
+          estimate.send("#{key}_postal_code=", value['postal_code'])
+          estimate.send("#{key}_note=", value['note'])
+        elsif value.class == Qbxml::Hash
+          value.each  do |k, v|
+            if k == 'list_id' || k == 'txn_id'
+              name = key.sub(/ref$/, "id")
+              name.sub!(/ret$/, "id")
+              estimate.send("#{name}=", v)
+            end  # end if k == 'list_id' || k == 'txn_id'
+          end # end value.each do |k,v|
+        elsif columns.include?(key.to_s)
+            estimate.send("#{key}=", value)
+        end # end if parsing ridiculousness
+      end # end for each |key, value|
       if estimate.save
-         Rails.logger.info("great success")
+        Rails.logger.info("great success")
       else
-         Rails.logger.info(estimate.errors)
-      end # end conditional
-     end # end for each response item
-  end # end method
-
-end
+        Rails.logger.info("Not saved:  #{estimate.errors}")
+      end # end if estimate save
+    end # end for each estimate
+  end # end handle response
+end # end worker class
--- lib/generators/worker/templates/ListWorker.rb
+++ lib/generators/worker/templates/ListWorker.rb
@@ -9,25 +9,43 @@ class List<%=name.classify%>Worker < QBWC::Worker
     }
   end
 
-  # There's no way this will work yet but .... getting there hopefully
   def handle_response(response, session, job, request, data)
-    # handle_response will get customers in groups of 100. When this is 0, we're done.
+    # handle_response will get <%=name.underscore%>s in groups of 100. When this is 0, we're done.
     complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
     columns = <%=name.classify%>.column_names
     response['<%=name.underscore%>_ret'].each do |qb|
       id = qb['list_id'] || qb['txn_id']
       <%=name.underscore%> = <%=name.classify%>.find_or_initialize_by(:id => id)
       qb.to_hash.each do |key, value|
-        <%=name.underscore%>.send("#{key}=", value) if columns.include?(key.to_s)
-        if key.match /ref_$/
-          <%=name.underscore%>.send("#{key.sub('ref_', '')}=", value[id])
-        end
-      end
+        if key.match /block$|xml_attributes/
+            next
+        elsif key.match /ship_address$|bill_address$/
+          <%=name.underscore%>.send("#{key}_addr1=", value['addr1'])
+          <%=name.underscore%>.send("#{key}_addr2=", value['addr2'])
+          <%=name.underscore%>.send("#{key}_addr3=", value['addr3'])
+          <%=name.underscore%>.send("#{key}_addr4=", value['addr4'])
+          <%=name.underscore%>.send("#{key}_addr5=", value['addr5'])
+          <%=name.underscore%>.send("#{key}_city=", value['city'])
+          <%=name.underscore%>.send("#{key}_state=", value['state'])
+          <%=name.underscore%>.send("#{key}_postal_code=", value['postal_code'])
+          <%=name.underscore%>.send("#{key}_note=", value['note'])
+        elsif value.class == Qbxml::Hash
+          value.each  do |k, v|
+            if k == 'list_id' || k == 'txn_id'
+              name = key.sub(/ref$/, "id")
+              name.sub!(/ret$/, "id")
+              <%=name.underscore%>.send("#{name}=", v)
+            end  # end if k == 'list_id' || k == 'txn_id'
+          end # end value.each do |k,v|
+        elsif columns.include?(key.to_s)
+            <%=name.underscore%>.send("#{key}=", value)
+        end # end if parsing ridiculousness
+      end # end for each |key, value|
       if <%=name.underscore%>.save
         Rails.logger.info("great success")
       else
-        Rails.logger.info(<%=name%>.errors)
-      end
-     end
-  end
-end
+        Rails.logger.info("Not saved:  #{<%=name.underscore%>.errors}")
+      end # end if <%=name.underscore%> save
+    end # end for each <%=name.underscore%>
+  end # end handle response
+end # end worker class
