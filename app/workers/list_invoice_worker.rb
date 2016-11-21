9cb90474ae82a3ac9e3e76d1a91581ae87d4ca6c
nklerk <nklerk@alopias.com>
2016-11-20 21:47:42 -0800

    test

--- app/workers/list_invoice_worker.rb
+++ app/workers/list_invoice_worker.rb
@@ -12,16 +12,15 @@ class ListInvoiceWorker < QBWC::Worker
   def handle_response(response, session, job, request, data)
     # handle_response will get invoices in groups of 100. When this is 0, we're done.
     complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
-    Rails.logger.warn(response)
     response['invoice_ret'].each do |qb|
-          Rails.logger.warn(qb)
+      Rails.logger.warn(qb.class)
 
-      invoice = Invoice.find_or_create_by(qb.to_hash)
-      if invoice.save
-        Rails.logger.info("great success")
-      else
-        Rails.logger.info("Not saved:  #{invoice.errors}")
-      end # end if invoice save
+      # invoice = Invoice.find_or_create_by(qb.to_hash)
+      # if invoice.save
+      #   Rails.logger.info("great success")
+      # else
+      #   Rails.logger.info("Not saved:  #{invoice.errors}")
+      # end # end if invoice save
     end # end for each invoice
   end # end handle response
 end # end worker class
