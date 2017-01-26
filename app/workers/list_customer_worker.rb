411d71e8525e85c917e61088df5962fa1573c781
nklerk <nklerk@alopias.com>
2017-01-26 15:41:20 -0800

    Follow Intuit non-intuitive enum type rules

--- app/workers/list_customer_worker.rb
+++ app/workers/list_customer_worker.rb
@@ -6,7 +6,7 @@ class ListCustomerWorker < QBWC::Worker
     {
       :customer_query_rq => {
         :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
-        :active_status => 'Active',
+        :active_status => 'ActiveOnly',
         :max_returned => 100
       }
     }
