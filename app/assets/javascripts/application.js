29aed7d295665af4cde05eb5ae219a30cec6a958
nicholejeannine <nklerk@alopias.com>
2017-01-29 18:05:35 -0800

    Use bootstrap sass gem and remove files that arent being customized

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,7 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
+//= require bootstrap
 //= require_tree .
 $(document).on("click",  ".navHoriz", function() {
 	$(".selected").removeClass('selected');
