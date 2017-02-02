9a5b19c5dee7bb2bdcbb5ba0d9106db4d7e9176d
nicholejeannine <nklerk@alopias.com>
2017-02-01 16:30:14 -0800

    Test unless complete in estimate list worker

--- app/workers/list_estimate_worker.rb
+++ app/workers/list_estimate_worker.rb
@@ -16,54 +16,55 @@ class ListEstimateWorker < QBWC::Worker
 
   def handle_response(response, session, job, request, data)
     # handle_response will get estimates in groups of 100. When this is 0, we're done.
-    complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
-    columns = Estimate.column_names
-    response['estimate_ret'].each do |qb|
-      estimate_id = qb['txn_id']
-      estimate = Estimate.find_or_initialize_by(:id => estimate_id)
-     # if qb['estimate_line_ret'].present?
-        # FIXME: will this even work???
-       # Rails.logger.info("Class of qb['estimate_line_ret'] is #{qb['estimate_line_ret'].class}")
-        ### FIXME:  CHECK RETURN TYPE OF ESTIMATE LINE - IT MAY ACTUALLY BE A REPEATED LIST, NOT AN ARRAY AS WE'D ASSUME .... MAYBE COUNT # OF INSTANCES, OR JUST RETURN THE TYPE?
-      #   qb['estimate_line_ret'].each do |line|
-      #     estimate_line = EstimateLine.find_or_initialize_by(:id => line['txn_line_id'])
-      #     estimate_line.send("estimate_id=", estimate_id)
-      #     if estimate_line.save
-      #       Rails.logger.info("Saved line: #{line}")
-      #     else
-      #       Rails.logger.info("#{estimate_line.errors}")
-      #     end
-      # end # end each estimate line
-    #end # end if estimate['estimate_line_ret'].present?
-      qb.to_hash.each do |key, value|
-        if columns.include?(key.to_s)
-          estimate.send("#{key}=", value)
-            next
-        elsif key.match /ship_address$|bill_address$|block$/
-            estimate.send("#{key}_addr1=", value['addr1'])
-            estimate.send("#{key}_addr2=", value['addr2'])
-            estimate.send("#{key}_addr3=", value['addr3'])
-            estimate.send("#{key}_addr4=", value['addr4'])
-            estimate.send("#{key}_addr5=", value['addr5'])
-            # Address blocks don't have city/state/postal)code/country/note
-            if key.match /address$/
-              estimate.send("#{key}_city=", value['city'])
-              estimate.send("#{key}_state=", value['state'])
-              estimate.send("#{key}_postal_code=", value['postal_code'])
-              estimate.send("#{key}_country=", value['country'])
-              estimate.send("#{key}_note=", value['note'])
-            end
-        elsif key.remove(/_ref$/).match /customer$|template$|terms$|sales_rep$|item_sales_tax$|customer_msg$|customer_sales_tax_code$/
-          name = key.remove(/_ref$/)
-          estimate.send("#{name}_id=", value['list_id'])
-          estimate.send("#{name}_full_name=", value['full_name'])
-        end # end if statement
-      end # end for each |key, value|
-      if estimate.save
-        Rails.logger.info("saved an estimate")
-      else
-        Rails.logger.info("Not saved:  #{estimate.errors}")
-      end # end if estimate save
-    end # end for each estimate
+    unless response['xml_attributes']['iteratorRemainingCount'] == '0'
+      columns = Estimate.column_names
+      response['estimate_ret'].each do |qb|
+        estimate_id = qb['txn_id']
+        estimate = Estimate.find_or_initialize_by(:id => estimate_id)
+       # if qb['estimate_line_ret'].present?
+          # FIXME: will this even work???
+         # Rails.logger.info("Class of qb['estimate_line_ret'] is #{qb['estimate_line_ret'].class}")
+          ### FIXME:  CHECK RETURN TYPE OF ESTIMATE LINE - IT MAY ACTUALLY BE A REPEATED LIST, NOT AN ARRAY AS WE'D ASSUME .... MAYBE COUNT # OF INSTANCES, OR JUST RETURN THE TYPE?
+        #   qb['estimate_line_ret'].each do |line|
+        #     estimate_line = EstimateLine.find_or_initialize_by(:id => line['txn_line_id'])
+        #     estimate_line.send("estimate_id=", estimate_id)
+        #     if estimate_line.save
+        #       Rails.logger.info("Saved line: #{line}")
+        #     else
+        #       Rails.logger.info("#{estimate_line.errors}")
+        #     end
+        # end # end each estimate line
+      #end # end if estimate['estimate_line_ret'].present?
+        qb.to_hash.each do |key, value|
+          if columns.include?(key.to_s)
+            estimate.send("#{key}=", value)
+              next
+          elsif key.match /ship_address$|bill_address$|block$/
+              estimate.send("#{key}_addr1=", value['addr1'])
+              estimate.send("#{key}_addr2=", value['addr2'])
+              estimate.send("#{key}_addr3=", value['addr3'])
+              estimate.send("#{key}_addr4=", value['addr4'])
+              estimate.send("#{key}_addr5=", value['addr5'])
+              # Address blocks don't have city/state/postal)code/country/note
+              if key.match /address$/
+                estimate.send("#{key}_city=", value['city'])
+                estimate.send("#{key}_state=", value['state'])
+                estimate.send("#{key}_postal_code=", value['postal_code'])
+                estimate.send("#{key}_country=", value['country'])
+                estimate.send("#{key}_note=", value['note'])
+              end
+          elsif key.remove(/_ref$/).match /customer$|template$|terms$|sales_rep$|item_sales_tax$|customer_msg$|customer_sales_tax_code$/
+            name = key.remove(/_ref$/)
+            estimate.send("#{name}_id=", value['list_id'])
+            estimate.send("#{name}_full_name=", value['full_name'])
+          end # end if statement
+        end # end for each |key, value|
+        if estimate.save
+          Rails.logger.info("saved an estimate")
+        else
+          Rails.logger.info("Not saved:  #{estimate.errors}")
+        end # end if estimate save
+      end # end for each estimate
+    end # end unless response == 0
   end # end handle response
 end # end class
