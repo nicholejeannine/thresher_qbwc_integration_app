890629fe12e3775bb970b9d8b412d551927590f1
nicholejeannine <nklerk@alopias.com>
2017-02-06 16:31:30 -0800

    Test craziness

--- app/workers/list_estimate_worker.rb
+++ app/workers/list_estimate_worker.rb
@@ -53,7 +53,7 @@ class ListEstimateWorker < QBWC::Worker
         Rails.logger.info("Not saved:  #{estimate.errors}")
       end # end if estimate save
       if qb['estimate_line_ret'].present?
-        qb['estimate_line_ret'].to_hash.each do |line|
+        qb['estimate_line_ret'].to_qbxml.each do |line|
           # At this point, the returned value is either an array or a hash
           # if line.class == Array
             Rails.logger.info("#{line.inspect}")
