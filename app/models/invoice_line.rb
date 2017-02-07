3be48dcff1504f891d76fc524f9d80a3d4b06382
nicholejeannine <nklerk@alopias.com>
2017-02-06 20:46:15 -0800

    Correct typo in comments

--- app/models/invoice_line.rb
+++ app/models/invoice_line.rb
@@ -1,6 +1,6 @@
 class InvoiceLine < ApplicationRecord
    self.primary_key = :id # In case customer model represents a mysql view
-   # set optional to true to prevent the database foreign-key checks on quickbooks-created rows
+   # set optional to true to prevent database foreign-key checks on quickbooks-created rows
    belongs_to :invoice, optional: true
 end
 
--- app/models/sales_order_line.rb
+++ app/models/sales_order_line.rb
@@ -1,6 +1,6 @@
 class SalesOrderLine < ApplicationRecord
  self.primary_key = :id # In case customer model represents a mysql view
- # set optional to true to prevent the database foreign-key checks on quickbooks-created rows
+ # set optional to true to prevent database foreign-key checks on quickbooks-created rows
  belongs_to :sales_order, optional: true
 end
 
