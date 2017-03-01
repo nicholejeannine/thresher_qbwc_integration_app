54b64d562903fe58f6d4e2cc12c8a56c4ccae23e
nicholejeannine <nklerk@alopias.com>
2017-03-01 12:58:53 -0800

    Test

--- app/workers/concerns/qb_list_type_worker.rb
+++ app/workers/concerns/qb_list_type_worker.rb
@@ -17,8 +17,8 @@ module QbListTypeWorker
 	 	def handle_response(response, session, job, request, data)
 			complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
 			columns = self.class.column_names
-    		response[response_name].to_a.each do |qb|
-    			instance = klass.find_or_initialize_by(:id => qb['list_id'])
+    		response[self.response_name].to_a.each do |qb|
+    			instance = self.klass.find_or_initialize_by(:id => qb['list_id'])
     			qb.to_hash.each do |key, value|
     			if columns.include?(key.to_s)
           			instance.send("#{key}=", value)
