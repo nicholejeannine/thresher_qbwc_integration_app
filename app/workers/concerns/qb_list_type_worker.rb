c6202fda5925c6a91ad6a4d963b94ec1f57ba26c
nicholejeannine <nklerk@alopias.com>
2017-03-01 12:00:54 -0800

    Test

--- app/workers/concerns/qb_list_type_worker.rb
+++ app/workers/concerns/qb_list_type_worker.rb
@@ -10,20 +10,21 @@ module QbListTypeWorker
 		    klass.to_s.underscore << '_ref'
 		end
 
-                def self.column_names
+        def self.column_names
 		    klass.column_names
-                end
+        end
+
 	 	def handle_response(response, session, job, request, data)
 			complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
-			columns = self.column_names
-    		response[self.response_name].to_a.each do |qb|
-    			instance = self.klass.find_or_initialize_by(:id => qb['list_id'])
+			columns = column_names
+    		response[response_name].to_a.each do |qb|
+    			instance = klass.find_or_initialize_by(:id => qb['list_id'])
     			qb.to_hash.each do |key, value|
     			if columns.include?(key.to_s)
           			instance.send("#{key}=", value)
           		end
           		end # end qb.to_hash.each do |key, value|
-          		if self.klass.save
+          		if klass.save
         			Rails.logger.info("saved it")
       			else
         			Rails.logger.info("Not saved")
