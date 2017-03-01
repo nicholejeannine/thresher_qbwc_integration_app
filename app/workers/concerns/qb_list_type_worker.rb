c1736e57de64e5826b851fef5610cac9c83132bc
bitadmin <bitadmin@alopias.com>
2017-03-01 13:16:37 -0800

    Changing ref to ret works .... helps to get the names right. Doh.

--- app/workers/concerns/qb_list_type_worker.rb
+++ app/workers/concerns/qb_list_type_worker.rb
@@ -7,7 +7,7 @@ module QbListTypeWorker
 		end
 
 		def self.response_name
-		    klass.to_s.underscore << '_ref'
+		    klass.to_s.underscore << '_ret'
 		end
 
         def self.column_names
@@ -17,14 +17,14 @@ module QbListTypeWorker
 	 	def handle_response(response, session, job, request, data)
 			complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
 			columns = self.class.column_names
-    		response[self.class.response_name].to_a.each do |qb|
+    		response["#{self.class.response_name}"].to_a.each do |qb|
     			instance = self.class.klass.find_or_initialize_by(:id => qb['list_id'])
     			qb.to_hash.each do |key, value|
     			if columns.include?(key.to_s)
           			instance.send("#{key}=", value)
           		end
           		end # end qb.to_hash.each do |key, value|
-          		if self.class.klass.save
+          		if instance.save
         			Rails.logger.info("saved it")
       			else
         			Rails.logger.info("Not saved")
