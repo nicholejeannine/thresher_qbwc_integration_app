1e0b550931eb8fd5068a410c1ceb0df23efa31e5
bitadmin <bitadmin@alopias.com>
2017-03-01 11:47:18 -0800

    Revised test still fails. May revert.

--- app/workers/concerns/qb_list_type_worker.rb
+++ app/workers/concerns/qb_list_type_worker.rb
@@ -2,25 +2,29 @@ module QbListTypeWorker
 	extend ActiveSupport::Concern
 
 	included do
-		def klass
-			self.name.remove(/^List/).remove(/Worker$/).constantize
+		def self.klass
+		   self.name.remove(/^List/).remove(/Worker$/).constantize
 		end
 
-		def response_name
-			klass.name.underscore << '_ref'
+		def self.response_name
+		    klass.to_s.underscore << '_ref'
 		end
+
+                def self.column_names
+		    klass.column_names
+                end
 
 		def handle_response(response, session, job, request, data)
 			complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
-			columns = klass.column_names
-    		response[response_name].to_a.each do |qb|
-    			instance = klass.find_or_initialize_by(:id => qb['list_id'])
+			columns = self.column_names
+    		response[self.response_name].to_a.each do |qb|
+    			instance = self.klass.find_or_initialize_by(:id => qb['list_id'])
     			qb.to_hash.each do |key, value|
     			if columns.include?(key.to_s)
           			instance.send("#{key}=", value)
           		end
           		end # end qb.to_hash.each do |key, value|
-          		if klass.save
+          		if self.klass.save
         			Rails.logger.info("saved it")
       			else
         			Rails.logger.info("Not saved")
