be37fb09de609ec7ee7e7c5b07251f7cb736cffa
nicholejeannine <nklerk@alopias.com>
2017-03-08 10:37:47 -0800

    Use double quotes

--- app/workers/customer_query_worker.rb
+++ app/workers/customer_query_worker.rb
@@ -5,9 +5,9 @@ class CustomerQueryWorker < QBWC::Worker
   def requests(job, session, data)
     {
       :customer_query_rq => {
-          :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
+          :xml_attributes => { "requestID" =>"1", "iterator"  => "Start" },
         :max_returned => 100,
-        :active_status => 'All',
+        :active_status => "All",
           #:from_modified_date => "#{QBWC::ActiveRecord::Job::QbwcJob.where(:name => 'customer_query').first.updated_at.localtime.strftime '%FT%R'}"
         :from_modified_date => "2017-02-23T08:14",
         :owner_id => 0
