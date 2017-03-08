ed54d27446912773a6571d931b7a57486c1f3f03
nklerk <nklerk@alopias.com>
2017-03-07 18:06:37 -0800

    Mighta broken the estimates....

--- app/workers/concerns/qb_txn_type_worker.rb
+++ app/workers/concerns/qb_txn_type_worker.rb
@@ -8,24 +8,29 @@ module QbTxnTypeWorker
 	  complete = response['xml_attributes']['iteratorRemainingCount'] == '0'
 	  response["#{self.class.response_name}"].to_a.each do |qb|
       instance_id = qb['txn_id']
-	    instance = self.class.klass.find_or_initialize_by(:id => instance_id)
+	    instance = klass.find_or_initialize_by(:id => instance_id)
 	    qb.to_hash.each do |key, value|
-          handle_hash(instance, key, value)
+        if klass.column_names.include?(key.to_s)
+          instance.send("#{key}=", "#{value}")
+        elsif address?(key)
+          handle_address(instance, key, value)
+        elsif ref_type?(key)
+          handle_ref_type(instance, key, value)
         end # end qb.to_hash.each do |key, value|
         if instance.save
           Rails.logger.info("Complete")
       	else
           Rails.logger.info("#{instance.errors}")
       	end # end if instance save
-        if qb["#{self.class.line_item_response_name}"].class == Array
-        qb["#{self.class.line_item_response_name}"].each do |line|
-          instance_line = self.class.line_klass.find_or_initialize_by(:id => line['txn_line_id'])
-          instance_line.send("#{self.class.klass.to_s.underscore}_id=", instance_id)
+        if qb["#{line_item_response_name}"].class == Array
+        qb["#{line_item_response_name}"].each do |line|
+          instance_line = line_klass.find_or_initialize_by(:id => line['txn_line_id'])
+          instance_line.send("#{klass.to_s.underscore}_id=", instance_id)
           line.to_hash.each do |k, v|
             if line_columns.include?(k.to_s)
               instance_line.send("#{k}=", v)
             elsif ref_type?(k)
-              handle_ref_type(instance_line, k, v, true)
+              handle_ref_type(instance_line, k, v)
             end # end key matching logic statements for instance_line if Array
           end # end line.to_hash.each do |k,v|
           if instance_line.save
@@ -35,14 +40,14 @@ module QbTxnTypeWorker
           end # end if instance_line.save
         end # end each qb['instance_line_ret'].each do |line|
       end # end if qb['instance_line_ret'].class == Array
-      if qb["#{self.class.line_item_response_name}"].class == Qbxml::Hash
-        instance_line = self.class.line_klass.find_or_initialize_by(:id => qb["#{self.class.line_item_response_name}"]['txn_line_id'])
-        instance_line.send("#{self.class.klass.to_s.underscore}_id=", instance_id)
-        qb["#{self.class.line_item_response_name}"].to_hash.each do |k, v|
+      if qb["#{line_item_response_name}"].class == Qbxml::Hash
+        instance_line = line_klass.find_or_initialize_by(:id => qb["#{line_item_response_name}"]['txn_line_id'])
+        instance_line.send("#{klass.to_s.underscore}_id=", instance_id)
+        qb["#{line_item_response_name}"].to_hash.each do |k, v|
           if line_columns.include?(k.to_s)
             instance_line.send("#{k}=", v)
           elsif ref_type?(k)
-           handle_ref_type(instance_line, k, v, true)
+           handle_ref_type(instance_line, k, v)
           end # end key matching logic statements for estimate_line if Qbxml::Hash
        end # end qb['estimate_line_ret'].to_hash.each do |k,v|
         if instance_line.save
