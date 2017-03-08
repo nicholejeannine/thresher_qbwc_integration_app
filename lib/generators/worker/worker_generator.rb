d84bdb5363a24e9bb4f88521b54f26446b1a1254
nicholejeannine <nklerk@alopias.com>
2017-03-07 23:42:24 -0800

    Remove front-end gems and tasks

--- lib/generators/worker/worker_generator.rb
+++
@@ -1,9 +0,0 @@
-class WorkerGenerator < Rails::Generators::NamedBase
-  source_root File.expand_path('../templates', __FILE__)
-
-  # id_type should be list_id or txn_id
-  def copy_list_worker
-    template('ListWorker.rb', "app/workers/list_#{name.underscore}_worker.rb", {'name' => name })
-  end
-
-end
