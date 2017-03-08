1461ab9514932e2ae7bfe5d24bb160bdda25e684
bitadmin <bitadmin@alopias.com>
2017-03-08 10:40:30 -0800

    Test integer

--- app/workers/customer_query_worker.rb
+++ app/workers/customer_query_worker.rb
@@ -5,7 +5,7 @@ class CustomerQueryWorker < QBWC::Worker
   def requests(job, session, data)
     {
       :customer_query_rq => {
-          :xml_attributes => { "requestID" =>"1", "iterator"  => "Start" },
+          :xml_attributes => { "requestID" =>1, "iterator"  => "Start" },
         :max_returned => 100,
         :active_status => "All",
           #:from_modified_date => "#{QBWC::ActiveRecord::Job::QbwcJob.where(:name => 'customer_query').first.updated_at.localtime.strftime '%FT%R'}"
