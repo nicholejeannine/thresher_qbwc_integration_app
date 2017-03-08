6b2c4a29f81efb35435054d9d1363373eb9fef34
nicholejeannine <nklerk@alopias.com>
2017-03-08 10:25:43 -0800

    Change name of job to query customers

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,24 +1,8 @@
-# Don't seed anything in production! Leave the data there alone!
-unless Rails.env.production?
- connection = ActiveRecord::Base.connection
- # load data from data file if it exists
-  if File.exists?('db/data/data.sql')
-    sql = File.read('db/data/data.sql')
-    statements = sql.split(/;$/)
-    statements.pop
-    ActiveRecord::Base.transaction do
-      statements.each do |statement|
-        connection.execute(statement)
-      end
-    end
-  end
- # User.create([{id: 1, email: 'nklerk@alopias.com', username: 'nklerk', password: 'password'}])
-
- # clear quickbooks jobs
+# clear quickbooks jobs
   QBWC.clear_jobs
-  QBWC.add_job(:list_customers, true, '', CustomerQueryWorker)
+  QBWC.add_job(:customer_query, true, '', CustomerQueryWorker)
  # QBWC.add_job(:list_estimates, true, '', EstimateQueryWorker)
  # QBWC.add_job(:list_sales_orders, true, '', SalesOrderQueryWorker)
  # QBWC.add_job(:list_purchase_orders, true, '', PurchaseOrderQueryWorker)
  # QBWC.add_job(:list_invoices, true, '', InvoiceQueryWorker)
-end
+
