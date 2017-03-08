42b375d51a537b486987b517b0aa83b333f065f6
nicholejeannine <nklerk@alopias.com>
2017-03-08 12:42:41 -0800

    Include QuickbooksQueryable module in other things so they will response to parse_qb_hash method

--- app/models/invoice_line.rb
+++ app/models/invoice_line.rb
@@ -1,6 +1,5 @@
 class InvoiceLine < ApplicationRecord
-   self.primary_key = :id # In case customer model represents a mysql view
-   # set optional to true to prevent database foreign-key checks on quickbooks-created rows
+   include QuickbooksQueryable
    belongs_to :invoice, optional: true
 end
 
--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -1,6 +1,5 @@
 class SalesOrder < ApplicationRecord
-   self.primary_key = :id # In case customer model represents a mysql view
-   # set optional to true to prevent the database foreign-key checks on quickbooks-created rows
+   include QuickbooksQueryable
    belongs_to :customer, optional: true
    has_many :sales_order_lines
 end
