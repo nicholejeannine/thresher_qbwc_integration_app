980115e59e61ff842d763ddabe2a7e3014ee96bd
bitadmin <bitadmin@alopias.com>
2017-03-07 23:25:12 -0800

    Fix merge conflict

--- app/workers/list_customer_worker.rb
+++ app/workers/list_customer_worker.rb
@@ -8,11 +8,14 @@ class ListCustomerWorker < QBWC::Worker
           :xml_attributes => { "requestID" =>"1", 'iterator'  => "Start" },
         :max_returned => 100,
         :active_status => 'All',
-          #:from_modified_date => "#{QBWC::ActiveRecord::Job::QbwcJob.where(:name => 'list_customers').first.updated_at.localtime.strftime '%FT%R'}",
+          #:from_modified_date => "#{QBWC::ActiveRecord::Job::QbwcJob.where(:name => 'list_customers').first.updated_at.localtime.strftime '%FT%R'}"
+        :from_modified_date => "2017-02-23T08:14",
         :owner_id => 0
       }
     }
   end
 
 	# see QbListTypeWorker for handle_response method
-end
+
+end # end class
+
