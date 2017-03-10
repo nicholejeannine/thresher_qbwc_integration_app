d1613662b15d15e7dca6008d60cc0434ce473f61
nicholejeannine <nklerk@alopias.com>
2017-03-09 18:08:20 -0800

    Replace

--- app/models/concerns/quickbooks_model.rb
+++ app/models/concerns/quickbooks_model.rb
@@ -32,38 +32,38 @@ module QuickbooksModel
 
 	# Handle each piece of an address
 	def handle_address(key, value)
-		persist("#{key}_addr1=", value['addr1'])
-		persist("#{key}_addr2=", value['addr2'])
-		persist("#{key}_addr3=", value['addr3'])
-		persist("#{key}_addr4=", value['addr4'])
-		persist("#{key}_addr5=", value['addr5'])
+		self.send("#{key}_addr1=", value['addr1']) if self.respond_to?("#{key}_addr1=")
+		self.send("#{key}_addr2=", value['addr2']) if self.respond_to?("#{key}_addr2=")
+		self.send("#{key}_addr3=", value['addr3']) if self.respond_to?("#{key}_addr3=")
+		self.send("#{key}_addr4=", value['addr4']) if self.respond_to?("#{key}_addr4=")
+		self.send("#{key}_addr5=", value['addr5']) if self.respond_to?("#{key}_addr5=")
 		# If this is an extended address we need to also save city, state, etc
 		if extended_address?(key)
-			persist("#{key}_city=", value['city'])
-			persist("#{key}_state=", value['state'])
-			persist("#{key}_postal_code=", value['postal_code'])
-			persist("#{key}_country=", value['country'])
-			persist("#{key}_note=", value['note'])
+			self.send("#{key}_city=", value['city']) if self.respond_to?("#{key}_city=")
+			self.send("#{key}_state=", value['state']) if self.respond_to?("#{key}_state=")
+			self.send("#{key}_postal_code=", value['postal_code']) if self.respond_to?("#{key}_postal_code=")
+			self.send("#{key}_country=", value['country'])if self.respond_to?("#{key}_country=")
+			self.send("#{key}_note=", value['note']) if self.respond_to?("#{key}_note=")
 		end
 	end
 
 	# Handle reference types - save the "list_id" and "full_name" values, only if those fields are in the DB
 	def handle_ref_type(key, value)
 		name = key.remove(/_ref$/)
-		persist("#{name}_id=", value['list_id'])
-		persist("#{name}_full_name=", value['full_name'])
+		self.send("#{name}_id=", value['list_id']) if self.respond_to?("#{name}_id=")
+		self.send("#{name}_full_name=", value['full_name']) if self.respond_to?("#{name}_full_name=")
 	end
 
 	# Customer objects have custom fields - this method parses the value returned for this part of a qb hash
 	def handle_custom_type(value)
 		value.to_a.each do |arr|
-			persist("primary_contact", "#{arr['data_ext_value']}") if arr['data_ext_name'] == 'Site Contact'
-			persist("primary_email", "#{arr['data_ext_value']}") if arr['data_ext_name'] == 'Site Email'
-			persist("primary_phone", "#{arr['data_ext_value']}") if arr['data_ext_name'] == 'Site Phone'
+			if self.respond_to?("primary_contact=") && arr['data_ext_name'] == 'Site Contact'
+				self.send("primary_contact=", "#{arr['data_ext_value']}")
+			elsif self.respond_to?("primary_email=") && arr['data_ext_name'] == 'Site Email'
+				self.send("primary_email=", "#{arr['data_ext_value']}")
+			elsif self.respond_to?("primary_phone=") && arr['data_ext_name'] == 'Site Phone'
+				self.send("primary_phone=", "#{arr['data_ext_value']}")
+			end
 		end
-	end
-
-	def persist(key, value)
-		self.send("#{key}=", "#{value}") if self.respond_to("#{key}=")
 	end
 end
