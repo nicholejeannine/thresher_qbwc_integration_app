f01ce664d9636ad5a808e4507d6218c3083e2707
bitadmin <bitadmin@alopias.com>
2017-03-10 20:06:33 -0800

    Update seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 # clear quickbooks jobs
   QBWC.clear_jobs
- # QBWC.add_job(:customer_query, true, '', CustomerQueryWorker)
+  QBWC.add_job(:customer_query, true, '', CustomerQueryWorker)
   QBWC.add_job(:estimate_query, true, '', EstimateQueryWorker)
  # QBWC.add_job(:list_sales_orders, true, '', SalesOrderQueryWorker)
  # QBWC.add_job(:list_purchase_orders, true, '', PurchaseOrderQueryWorker)
