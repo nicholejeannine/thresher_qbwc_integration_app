bf979cf6b6c789c0af454ac7bdb180aebc37b113
nicholejeannine <nklerk@alopias.com>
2017-03-13 16:46:00 -0700

    Generators happening

--- lib/generators/query_worker/query_worker_generator.rb
+++ lib/generators/query_worker/query_worker_generator.rb
@@ -1,9 +1,18 @@
 class QueryWorkerGenerator < Rails::Generators::NamedBase
   source_root File.expand_path('../templates', __FILE__)
-  argument :args
   
   def generate
-    arguments = args.flat_map(&:to_s).join(" ")
-    in_root { run_ruby_script("bin/rails generate model #{name} #{arguments}", verbose: false) }
+    arguments = elements.flat_map(&:to_s).join(" ")
+    in_root { run_ruby_script("bin/rails generate model #{name} #{arguments} --parent=QuickbooksModel --no-migration", verbose: false) }
+    in_root { run_ruby_script("bin/rails generate migration Create#{name.classify} #{arguments}", verbose: false) }
   end
+
+  def copy_worker
+    template('query_worker.rb', "app/workers/#{name.underscore}_query_worker.rb")
+  end
+  
+  def elements
+    QBWC.parser.describe('ReceivePaymentRet').elements.map{|e|e.name.underscore}
+  end
+  
 end
---
+++ lib/generators/query_worker/templates/query_worker.rb
@@ -0,0 +1,16 @@
+class <%=name.classify%>QueryWorker < QBWC::Worker
+
+include QueryResponseHandler
+
+  def requests(job, session, data)
+    {
+      :<%=name.underscore%>_query_rq => {
+        :xml_attributes => { "requestID" =>"1", "iterator"  => "Start" },
+        :max_returned => 100,
+        :active_status => "All",
+        :from_modified_date => "2017-02-28T08:14",
+        #:from_modified_date => "#{QBWC::ActiveRecord::Job::QbwcJob.where(:name => '<%=name.underscore%>_query').first.updated_at.localtime.strftime '%FT%R'}"
+      }
+    }
+  end
+end
