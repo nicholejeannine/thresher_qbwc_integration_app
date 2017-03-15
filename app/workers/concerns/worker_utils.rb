1d779ab8df12f9b32382d66cc22a448e4d1333c5
nicholejeannine <nklerk@alopias.com>
2017-03-15 16:40:31 -0700

    Use a constant dummy

--- app/workers/concerns/worker_utils.rb
+++ app/workers/concerns/worker_utils.rb
@@ -8,7 +8,7 @@ module WorkerUtils
 
   # Retrieve the quickbooks xml response name from the worker class instance (e.g., "CustomerQueryWorker.new.klass returns 'customer_ret')
   def response_name
-    if klass == 'Client'
+    if klass == Client
       return 'customer_ret'
     else
       return klass.to_s.underscore << '_ret'
