b2354eb1b5d661a076a65b3c3db8b7f52684fd46
bitadmin <bitadmin@alopias.com>
2017-03-15 16:21:11 -0700

    Update seeds for test

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
   QBWC.clear_jobs
   
   QBWC.add_job(:customer_query, true, '', CustomerQueryWorker)
-  QBWC.add_job(:estimate_query, true, '', EstimateQueryWorker)
-  QBWC.add_job(:list_sales_orders, true, '', SalesOrderQueryWorker)
-  QBWC.add_job(:list_purchase_orders, true, '', PurchaseOrderQueryWorker)
-  QBWC.add_job(:list_invoices, true, '', InvoiceQueryWorker)
+ # QBWC.add_job(:estimate_query, true, '', EstimateQueryWorker)
+ # QBWC.add_job(:list_sales_orders, true, '', SalesOrderQueryWorker)
+ # QBWC.add_job(:list_purchase_orders, true, '', PurchaseOrderQueryWorker)
+ # QBWC.add_job(:list_invoices, true, '', InvoiceQueryWorker)
 #  QBWC.add_job(:list_receive_payments, true, '', ReceivePaymentQueryWorker)
   
