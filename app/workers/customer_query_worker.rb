eef829a57d5d1b5e1fe2bab5b6897b2db53bcc26
nicholejeannine <nklerk@alopias.com>
2017-03-15 16:21:07 -0700

    ?

--- app/workers/customer_query_worker.rb
+++ app/workers/customer_query_worker.rb
@@ -9,8 +9,7 @@ class CustomerQueryWorker < QBWC::Worker
         :max_returned => 100,
         :active_status => "All",
         :from_modified_date => "2017-02-28T08:14",
-        :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'FullName', 'IsActive', 'Sublevel', 'CompanyName', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'TotalBalance', 'SalesTaxCodeRep', 'ItemSalesTaxRef', 'AccountNumber', 'PreferredDeliveryMethod' ]
-        #:from_modified_date => "#{QBWC::ActiveRecord::Job::QbwcJob.where(:name => 'customer_query').first.updated_at.localtime.strftime '%FT%R'}",
+        :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'FullName', 'IsActive', 'Sublevel', 'CompanyName', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'TotalBalance', 'SalesTaxCodeRep', 'ItemSalesTaxRef', 'AccountNumber', 'PreferredDeliveryMethod'],
         :owner_id => 0
       }
     }
