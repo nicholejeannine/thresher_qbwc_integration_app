8ff7d9d8aeb51b004e6b82bcfa3229223dcd4494
nicholejeannine <nklerk@alopias.com>
2017-03-15 16:24:04 -0700

    Rename

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # clear quickbooks jobs
   QBWC.clear_jobs
   
-  QBWC.add_job(:customer_query, true, '', CustomerQueryWorker)
+  QBWC.add_job(:client_query, true, '', ClientQueryWorker)
  # QBWC.add_job(:estimate_query, true, '', EstimateQueryWorker)
  # QBWC.add_job(:list_sales_orders, true, '', SalesOrderQueryWorker)
  # QBWC.add_job(:list_purchase_orders, true, '', PurchaseOrderQueryWorker)
