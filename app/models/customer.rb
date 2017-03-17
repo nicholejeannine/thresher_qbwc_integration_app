15a190e24270077043ead47e44a6465bb43e7f58
nklerk <nklerk@alopias.com>
2017-03-17 02:45:20 -0700

    test override

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,6 +2,7 @@ class Customer < ApplicationRecord
 
   def self.parse_qb_response(qb)
     c = Client.find_or_initialize_by(:id => qb['list_id'])
+    c.full_name = qb['full_name']
     c.save
 
   end
