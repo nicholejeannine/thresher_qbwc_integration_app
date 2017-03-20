0ba34f3dc16186385bec9d5c892c71a345969fa0
bitadmin <bitadmin@alopias.com>
2017-03-20 15:31:06 -0700

    Update customer query

--- app/workers/query_worker.rb
+++ app/workers/query_worker.rb
@@ -6,15 +6,15 @@ class QueryWorker < QBWC::Worker
 
   def requests(job, session, data)
 	[
-	#{:customer_query_rq => {
-	   # :xml_attributes => { :requestID =>1, :iterator  => "Start" },
-	  #  :max_returned => 100,
-	   # :active_status => "All",
-	  #  :from_modified_date =>last_ran,
-	    # :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'FullName', 'IsActive', 'CompanyName', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'TotalBalance', 'SalesTaxCodeRep', 'ItemSalesTaxRef', 'AccountNumber', 'PreferredDeliveryMethod'],
-	    #:owner_id => 0
-		#}
-	  #},
+	{:customer_query_rq => {
+	    :xml_attributes => { :requestID =>1, :iterator  => "Start" },
+	    :max_returned => 100,
+	    :active_status => "All",
+	    :from_modified_date =>last_ran,
+	     :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'Name', 'FullName', 'IsActive', 'ParentRef', 'Sublevel', 'CompanyName', 'Salutation', 'FirstName', 'MiddleName', 'LastName', 'JobTitle', 'BillAddress', 'ShipAddress','Phone', 'AltPhone', 'Fax', 'Email', 'Cc', 'Contact', 'AltContact', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'Balance', 'TotalBalance', 'SalesTaxCodeRef', 'ItemSalesTaxRef', 'AccountNumber', 'JobStatus', 'JobStartDate', 'JobProjectedEndDate', 'JobEndDate', 'JobDesc', 'JobTypeRef', 'PreferredDeliveryMethod', 'DataExtRet'],
+	    :owner_id => 0
+	    }
+	  },
 	  {:estimate_query_rq => {
 	  	  :xml_attributes => { :requestID =>1, :iterator  => "Start" },
 	 	  :max_returned => 100,
