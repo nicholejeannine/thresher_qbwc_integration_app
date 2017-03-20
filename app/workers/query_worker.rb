73ada9d395660bc2178869cc439464ceff93afd2
nicholejeannine <nklerk@alopias.com>
2017-03-20 13:23:34 -0700

    test

--- app/workers/query_worker.rb
+++ app/workers/query_worker.rb
@@ -5,15 +5,16 @@ class QueryWorker < QBWC::Worker
   end
 
   def requests(job, session, data)
-	[{:customer_query_rq => {
-	    :xml_attributes => { :requestID =>1, :iterator  => "Start" },
-	    :max_returned => 100,
-	    :active_status => "All",
-	    :from_modified_date =>last_ran,
-	    # :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'FullName', 'IsActive', 'CompanyName', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'TotalBalance', 'SalesTaxCodeRep', 'ItemSalesTaxRef', 'AccountNumber', 'PreferredDeliveryMethod'],
-	    :owner_id => 0
-		}
-	  },
+	[
+	# {:customer_query_rq => {
+	#     :xml_attributes => { :requestID =>1, :iterator  => "Start" },
+	#     :max_returned => 100,
+	#     :active_status => "All",
+	#     :from_modified_date =>last_ran,
+	#     # :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'FullName', 'IsActive', 'CompanyName', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'TotalBalance', 'SalesTaxCodeRep', 'ItemSalesTaxRef', 'AccountNumber', 'PreferredDeliveryMethod'],
+	#     :owner_id => 0
+	# 	}
+	#   },
 	  {:estimate_query_rq => {
 	  	  :xml_attributes => { :requestID =>1, :iterator  => "Start" },
 	 	  :max_returned => 100,
