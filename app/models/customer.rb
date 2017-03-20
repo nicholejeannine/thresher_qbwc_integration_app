1726d5da37dbaca35947664a8ba27b83f299b7ad
bitadmin <bitadmin@alopias.com>
2017-03-20 10:15:16 -0700

    Parse type

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,9 +2,13 @@ class Customer < ApplicationRecord
 
 		self.abstract_class = true
 		def self.parse_qb_response(qb)
-			c = Client.find_or_initialize_by(:id => qb['list_id'])
-			c.full_name = qb['full_name']
+			klass = customer_type(qb['name'], qb['sublevel'])
+			c = klass.find_or_initialize_by(:id => qb['list_id'])
 			c.save
-
+		end
+		
+		def self.customer_type(name, sublevel)
+			sublevel == 0 ? Client : Job
 		end
 end
+
