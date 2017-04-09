9fe552d95c076e120987a202307d3df070067251
nicholejeannine <nklerk@alopias.com>
2017-04-09 00:38:01 -0700

    TODOs for estimate tests

--- test/models/estimate_test.rb
+++ test/models/estimate_test.rb
@@ -2,12 +2,36 @@ require 'test_helper'
 
 class EstimateTest < ActiveSupport::TestCase
   test "can parse a simple hash" do
-    e = prep({"txn_id" => '222',
-              "is_active" => 1
+    e = prep({"txn_id" => '222'
              })
     assert_equal(1, Estimate.count, "should equal one")
   end
-
+  
+  # TODO
+  test "saves customer id" do
+    skip
+  end
+  test "saves a bill address with estimate id" do
+    skip
+  end
+  test "saves a ship address with estimate id" do
+    skip
+  end
+  test "saves an estimate line that is passed in as a Qbsml Hash" do
+    skip
+  end
+  test "saves all estimate lines passed in as an array" do
+    skip
+  end
+  test "estimate ignores ignored types" do
+    skip
+  end
+  test "estimate handles ref type" do
+    skip
+  end
+  test "estimate lines handle ref type" do
+    skip
+  end
   def prep(hash)
     Estimate.destroy_all
     id = hash["txn_id"]
