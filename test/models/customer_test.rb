7ba85855370321de1bd7d6fae2044298533254f9
nicholejeannine <nklerk@alopias.com>
2017-04-09 00:19:26 -0700

    add a few more assertions and extra common setup stuff into a shared method

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -5,45 +5,52 @@ class CustomerTest < ActiveSupport::TestCase
   end
   
   test "can parse simple qb response" do
-      Customer.destroy_all
-      c = Customer.new(:id => '222')
-      c.parse_hash({"list_id" => '222',
+      c = prep({"list_id" => '222',
                     "is_active" => 1,
                     "sublevel" => 0,
                     "parent_id" => nil,
                     "name" => "Frankie",
                     "full_name" => "Frankie"
                    })
-      c.save
     assert_equal(1, Customer.count, "should equal one")
   end
   
   test "can save billing address" do
-    Customer.destroy_all
-    hash = {"list_id" => '333',
+    c = prep({"list_id" => '333',
             "bill_address" => {
                 "xml_attributes" => "some crap",
                 "addr1" => "333 Street"
-            }
-    }
-    c = Customer.new(:id => '333')
-    c.parse_hash(hash)
-    c.save
+            }})
     assert_equal("333", BillAddress.first.id, "Bill Address id should match id of customer hash")
   end
 
   test "can save ship address" do
-    Customer.destroy_all
-    hash = {"list_id" => '444',
+    c = prep({"list_id" => '444',
             "ship_address" => {
                 "xml_attributes" => "some crap",
                 "addr1" => "444 Street"
-            }
-    }
-    c = Customer.new(:id => '444')
+            }})
+    assert_equal("444", ShipAddress.first.id, "Bill Address id should match id of customer hash")
+    assert_equal("444 Street", ShipAddress.first.addr1, "Bill Address row value should match that of customer hash")
+  end
+  
+  test "can save contact info" do
+    c = prep({"list_id" => '555',
+           "salutation" => "Joe"})
+    assert_equal("555", Contact.first.id, "Contact id should match id of customer hash")
+    assert_equal("Joe", Contact.first.salutation, "Contact salutation should match salutation value of customer hash")
+  end
+  
+  # test "can parse data extensions"
+  
+  
+  def prep(hash)
+    Customer.destroy_all
+    id = hash["list_id"]
+    c = Customer.new(:id => id)
     c.parse_hash(hash)
     c.save
-    assert_equal("444", ShipAddress.first.id, "Bill Address id should match id of customer hash")
+    c
   end
 end
 
