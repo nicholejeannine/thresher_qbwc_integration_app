861536ba96190856d29f696fd5618c44739fb066
nicholejeannine <nklerk@alopias.com>
2017-04-10 07:20:59 -0700

    Test

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,21 +3,11 @@ class Contact < ApplicationRecord
   def self.handle_contact(hash, klass, id)
     begin
       contact_instance = Contact.find_or_initialize_by(:id => id, :contact_type => klass)
-      contact_instance.update_attribute('site_contact', nil)
-      contact_instance.update_attribute('site_email', nil)
-      contact_instance.update_attribute('site_phone', nil)
       hash.each do |k, v|
         if k.match(/data_ext_ret/)
-          # Customer objects have custom fields
-          v.each do |arr|
-            if arr['data_ext_name'] == 'Site Contact'
-              contact_instance.update_attribute("site_contact", arr['data_ext_value'])
-            elsif arr['data_ext_name'] == 'Site Email'
-              contact_instance.update_attribute("site_email", arr['data_ext_value'])
-            elsif arr['data_ext_name'] == 'Site Phone'
-              contact_instance.update_attribute("site_phone", arr['data_ext_value'])
-            end
-          end
+          contact_instance.site_contact = hash['data_ext_ret']&.select{|x|x.has_value?("Site Contact")}.map{|x|x.values_at("data_ext_value")}
+          contact_instance.site_phone = hash['data_ext_ret']&.select{|x|x.has_value?("Site Phone")}.map{|x|x.values_at("data_ext_value")}
+          contact_instance.site_email = hash['data_ext_ret']&.select{|x|x.has_value?("Site Email")}.map{|x|x.values_at("data_ext_value")}
         else
           contact_instance.update_attribute(k, v)
         end
