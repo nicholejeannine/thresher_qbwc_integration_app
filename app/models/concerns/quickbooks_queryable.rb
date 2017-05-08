98edafdb9db14ef58a7ab7fb991e364b3e96e074
nicholejeannine <nklerk@alopias.com>
2017-05-08 11:58:18 -0700

    Try this

--- app/models/concerns/quickbooks_queryable.rb
+++ app/models/concerns/quickbooks_queryable.rb
@@ -6,7 +6,7 @@ module QuickbooksQueryable
   def handle_ref_type(key, value)
     if key.match(/customer_ref|parent_ref/)
       update_attribute("#{key.remove(/ref$/).concat('id')}", value['list_id'])
-      #update_attribute("#{key.remove(/ref$/).concat('type')}", name_type(value['full_name'])) if key == 'customer_ref'
+      update_attribute("#{key.remove(/ref$/).concat('type')}", name_type(value['full_name'])) if key == 'customer_ref'
     else
       update_attribute("#{key.remove(/_ref$/)}", value['full_name'])
     end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,6 +5,8 @@ class Customer < ApplicationRecord
 			qb.merge!('data_ext_ret' => nil)
 		end
 		if qb['sublevel'] == 0 && !qb['name'].upcase.start_with?('P-')
+			client_exclusion_keys = ['name', 'parent_id', 'sublevel', 'job_status', 'job_start_date', 'job_projected_end_date', 'job_end_date', 'job_desc', 'job_type']
+			qb.extract!(*client_exclusion_keys)
 			Client.parse_qb_response(qb)
 		elsif qb['sublevel'] > 0 && !qb['name'].upcase.start_with?('P-')
 			Job.parse_qb_response(qb)
