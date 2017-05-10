d8b07e9869547af9c1ef1b8afd239393a2b9aeb2
nicholejeannine <nklerk@alopias.com>
2017-05-09 22:17:59 -0700

    Update worker to write to LinkedTxn table and normal tables and not the QbwcError table

--- app/workers/linked_txn_worker.rb
+++ app/workers/linked_txn_worker.rb
@@ -36,21 +36,14 @@ class LinkedTxnWorker < QBWC::Worker
   def handle_response(r, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
-    r['estimate_ret']&.each do |qb|
-      QbwcError.create(:worker_class => 'Estimate', :model_id => "#{qb['txn_id']}", :error_message => "#{qb['linked_txn']}") if qb.has_key?('linked_txn')
-      #Rails.logger.info("Estimate #{qb['txn_id']}: #{qb['linked_txn']}") if qb.has_key?('linked_txn')
-    end
-    r['sales_order_ret']&.each do |qb|
-      QbwcError.create(:worker_class => 'SalesOrder', :model_id => "#{qb['txn_id']}", :error_message => "#{qb['linked_txn']}") if qb.has_key?('linked_txn')
-      #Rails.logger.info("SalesOrder #{qb['txn_id']}: #{qb['linked_txn']}") if qb.has_key?('linked_txn')
-    end
-    r['purchase_order_ret']&.each do |qb|
-      QbwcError.create(:worker_class => 'PurchaseOrder', :model_id => "#{qb['txn_id']}", :error_message => "#{qb['linked_txn']}") if qb.has_key?('linked_txn')
-      #Rails.logger.info("PurchaseOrder #{qb['txn_id']}: #{qb['linked_txn']}") if qb.has_key?('linked_txn')
-    end
-    r['invoice_ret']&.each do |qb|
-      QbwcError.create(:worker_class => 'Invoice', :model_id => "#{qb['txn_id']}", :error_message => "#{qb['linked_txn']}") if qb.has_key?('linked_txn')
-      # Rails.logger.info("Invoice #{qb['txn_id']}: #{qb['linked_txn']}") if qb.has_key?('linked_txn')
+    begin
+      r['customer_ret']&.each{|qb|Customer.parse_customer_response(qb)}
+      r['estimate_ret']&.each{|qb|Estimate.parse_qb_response(qb)}
+      r['sales_order_ret']&.each{|qb|SalesOrder.parse_qb_response(qb)}
+      r['purchase_order_ret']&.each{|qb|PurchaseOrder.parse_qb_response(qb)}
+      r['invoice_ret']&.each{|qb|Invoice.parse_qb_response(qb)}
+    rescue Exception => e
+      QbwcError.create(:worker_class => self.class.name, :error_message => e)
     end
   end
 end
