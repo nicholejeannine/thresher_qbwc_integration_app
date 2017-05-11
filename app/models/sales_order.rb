5411a56a4d12cc3fc34f10f673e841c2fa9b9166
nicholejeannine <nklerk@alopias.com>
2017-05-11 11:56:20 -0700

    Add & for nil checks in Sales Order parse_memo callback

--- app/models/sales_order.rb
+++ app/models/sales_order.rb
@@ -7,7 +7,7 @@ class SalesOrder < ApplicationRecord
   
   def parse_memo
     begin
-    if self.estimate_id.nil? && memo.downcase.include?("estimate")
+    if self.estimate_id.nil? && memo&.downcase&.include?("estimate")
       ref = memo.try(:split)[1].remove(":")
       self.estimate_id = Estimate.where(:ref_number => ref).first&.id
     end
