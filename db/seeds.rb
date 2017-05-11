0f9e7016e8c59f8f7db58d9d1cab860db9db584b
nklerk <nklerk@alopias.com>
2017-05-10 23:52:02 -0700

    Set up initial worker in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # clear quickbooks jobs
   QBWC.clear_jobs
   # QBWC.add_job(:client, true, '', ClientWorker)
-#  QBWC.add_job(:initial, true, '', InitialWorker)
-   QBWC.add_job(:linked_txn, true, '', LinkedTxnWorker)
+   QBWC.add_job(:initial, true, '', InitialWorker)
+  # QBWC.add_job(:linked_txn, true, '', LinkedTxnWorker)
   # QBWC.add_job(:query, true, '', QueryWorker)
   # QBWC.add_job(:estimate_query, true, '', EstimateQueryWorker)
  # QBWC.add_job(:list_sales_orders, true, '', SalesOrderQueryWorker)
