e40c738abc35ff69e1dddaff7275bd4459c92ef2
nicholejeannine <nklerk@alopias.com>
2017-05-11 12:58:05 -0700

    List worker good to go more or less

--- app/workers/list_worker.rb
+++ app/workers/list_worker.rb
@@ -1,18 +1,18 @@
 class ListWorker < QBWC::Worker
   def last_ran
-    QBWC::ActiveRecord::Job::QbwcJob.where(:name => 'client').first&.updated_at&.localtime&.strftime '%FT%R'
+    QBWC::ActiveRecord::Job::QbwcJob.where(:name => 'list').first&.updated_at&.localtime&.strftime '%FT%R'
   end
   def requests(job, session, data)
     [
-        # {:customer_query_rq => {
-        #     :xml_attributes => { :requestID =>1, :iterator  => "Start" },
-        #     :max_returned => 100,
-        #     :active_status => "All",
-        #     :from_modified_date => last_ran,
-        #     :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'Name', 'FullName', 'IsActive', 'ParentRef', 'Sublevel', 'CompanyName', 'Salutation', 'FirstName', 'MiddleName', 'LastName', 'JobTitle', 'BillAddress', 'ShipAddress','Phone', 'AltPhone', 'Fax', 'Email', 'Cc', 'Contact', 'AltContact', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'Balance', 'TotalBalance', 'SalesTaxCodeRef', 'ItemSalesTaxRef', 'AccountNumber', 'JobStatus', 'JobStartDate', 'JobProjectedEndDate', 'JobEndDate', 'JobDesc', 'JobTypeRef', 'PreferredDeliveryMethod', 'DataExtRet'],
-        #     :owner_id => 0
-        # }
-        # },
+        {:customer_query_rq => {
+            :xml_attributes => { :requestID =>1, :iterator  => "Start" },
+            :max_returned => 100,
+            :active_status => "All",
+            :from_modified_date => last_ran,
+            :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'Name', 'FullName', 'IsActive', 'ParentRef', 'Sublevel', 'CompanyName', 'Salutation', 'FirstName', 'MiddleName', 'LastName', 'JobTitle', 'BillAddress', 'ShipAddress','Phone', 'AltPhone', 'Fax', 'Email', 'Cc', 'Contact', 'AltContact', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'Balance', 'TotalBalance', 'SalesTaxCodeRef', 'ItemSalesTaxRef', 'AccountNumber', 'JobStatus', 'JobStartDate', 'JobProjectedEndDate', 'JobEndDate', 'JobDesc', 'JobTypeRef', 'PreferredDeliveryMethod', 'DataExtRet'],
+            :owner_id => 0
+        }
+        },
          {:vendor_query_rq => {
              :xml_attributes => { :requestID =>1, :iterator  => "Start" },
              :max_returned => 100,
