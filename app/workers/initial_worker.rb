74587162cbf8779ca7eccf5747d38674d6f3c17f
nklerk <nklerk@alopias.com>
2017-05-10 23:44:42 -0700

    Add vendor query to initial worker

--- app/workers/initial_worker.rb
+++ app/workers/initial_worker.rb
@@ -1,5 +1,5 @@
 class InitialWorker < QBWC::Worker
- 
+
 	def requests(job, session, data)
 	[
 	{:customer_query_rq => {
@@ -32,22 +32,29 @@ class InitialWorker < QBWC::Worker
 		    :max_returned => 100,
 		    :include_line_items => true
 		  }
-		}]
+		  },
+		   {:vendor_query_rq => {
+       :xml_attributes => { :requestID =>1, :iterator  => "Start" },
+       :max_returned => 100,
+       :active_status => "All",
+       :from_modified_date => last_ran
+        }
+				}]
 	end
 
 
-	def handle_response(r, session, job, request, data)
-        # handle_response will get customers in groups of 100. When this is 0, we're done.
-		complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
-		begin
-		  r['customer_ret']&.each{|qb|Customer.parse_customer_response(qb)}
-          r['estimate_ret']&.each{|qb|Estimate.parse_qb_response(qb)}
-          r['sales_order_ret']&.each{|qb|SalesOrder.parse_qb_response(qb)}
-          r['purchase_order_ret']&.each{|qb|PurchaseOrder.parse_qb_response(qb)}
-          r['invoice_ret']&.each{|qb|Invoice.parse_qb_response(qb)}
-		rescue Exception => e
-		  QbwcError.create(:worker_class => self.class.name, :error_message => e)
-		end
-
-	end
+  def handle_response(r, session, job, request, data)
+  # handle_response will get customers in groups of 100. When this is 0, we're done.
+		 complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
+		 begin
+		   r['customer_ret']&.each{|qb|Customer.parse_customer_response(qb)}
+     r['estimate_ret']&.each{|qb|Estimate.parse_qb_response(qb)}
+     r['sales_order_ret']&.each{|qb|SalesOrder.parse_qb_response(qb)}
+     r['purchase_order_ret']&.each{|qb|PurchaseOrder.parse_qb_response(qb)}
+     r['invoice_ret']&.each{|qb|Invoice.parse_qb_response(qb)}
+     r['vendor_ret']&.each{|qb|Vendor.parse_qb_response(qb)}
+		 rescue Exception => e
+		   QbwcError.create(:worker_class => self.class.name, :error_message => e)
+		 end
+  end
 end
