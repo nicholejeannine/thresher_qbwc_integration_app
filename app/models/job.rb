6b7ac35b0ba74d38e7b5216bc817fd01baa8cbc8
nicholejeannine <nklerk@alopias.com>
2017-05-17 19:50:27 -0700

    Put a space in between 'NotAwarded' and 'InProgress' job status values (to display as 2 words)

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,4 +1,11 @@
 class Job < ApplicationRecord
   self.primary_key = :id
   include QuickbooksQueryable
+  before_save :titleize_job_status
+
+  private
+  # job_status fields come back like "InProgress" - make it save as two separate words.
+  def titleize_job_status
+    self.job_status = self.job_status.titleize
+  end
 end
