a8c4370575226accfe4871f74451ec04d4ed7f84
nicholejeannine <nklerk@alopias.com>
2017-06-05 14:15:03 -0700

    Titlize avoid nulls for projects

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,6 +6,6 @@ class Project < ApplicationRecord
   private
   # job_status fields come back like "InProgress" - make it save as two separate words.
   def titleize_job_status
-    self.job_status = self.job_status.titleize
+    self.job_status = self.job_status&.titleize
   end
 end
