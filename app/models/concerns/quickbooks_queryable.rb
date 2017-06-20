cfdd4e7634ea0564897a29c4e76927273a3bca7f
nicholejeannine <nklerk@alopias.com>
2017-06-20 13:08:57 -0700

    Test again

--- app/models/concerns/quickbooks_queryable.rb
+++ app/models/concerns/quickbooks_queryable.rb
@@ -53,9 +53,9 @@ module QuickbooksQueryable
   end
 
   def process_linked_txn(ret = nil)
-    return if (ret.nil? || !self.respond_to?(:parse_link))
-    self.parse_link(ret) if ret.is_a?(Qbxml::Hash)
-    ret.each{|link|self.parse_link(link) if ret.is_a?(Array)}
+    # return unless self.respond_to?(:parse_link))
+    parse_link(ret) if ret.is_a?(Qbxml::Hash)
+    ret.each{|link|parse_link(link) if ret.is_a?(Array)}
   end
   
   
