8d33d1ec8c783d4475baf4bfb6e460b07c877361
nicholejeannine <nklerk@alopias.com>
2017-06-25 18:11:20 -0700

    Reseed to test txn_worker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 # clear quickbooks jobs
   QBWC.clear_jobs
-  QBWC.add_job(:list, true, '', ListWorker)
+  QBWC.add_job(:list, false, '', ListWorker)
+  QBWC.add_job(:txn, true, '', TransactionQueryWorker)
   QBWC.add_job(:initial, false, '', InitialWorker)
   QBWC.add_job(:query, false, '', QueryWorker)
   QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
