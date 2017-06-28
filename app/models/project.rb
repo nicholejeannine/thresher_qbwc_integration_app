2b72d68c053b310a73421a340d0a981850b55cff
nicholejeannine <nklerk@alopias.com>
2017-06-27 18:08:18 -0700

    Do NOT allow new records to come in from the Quickbooks side - yay for the failing test, it means the code has been changed per requirements and test to be updated

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,13 +4,19 @@ class Project < ApplicationRecord
 
   def self.new_project(qb)
     begin
+      # First try to find by list id
     p =   Project.where(:list_id => qb['list_id'])&.first
-    p ||= Project.find_or_initialize_by(:full_name => qb['full_name'])
-    p.list_id= qb['list_id']
-    p.parse_hash(qb.to_hash)
-    p.save
+    # Then try to find by full name
+    p ||= Project.where(:full_name => qb['full_name'])&.first
+    # If it's not found, write to error log. If it's found, let it update.
+    if p.nil?
+      QbwcError.create!(:worker_class => "Project::No Match Found", :model_id => qb['list_id'], :error_message => "No project in the portal matches customer #{qb['full_name']} in Quickbooks")
+    else
+      p.parse_hash(qb.to_hash)
+      p.save
+    end
     rescue Exception => e
-      QbwcError.create(:worker_class => "#{self.name}", :model_id => "#{qb['list_id']}", :error_message => "Error parsing response: #{e}")
+      QbwcError.create(:worker_class => "Project", :model_id => "#{self.id}", :error_message => "#{e}")
     end
   end
   
