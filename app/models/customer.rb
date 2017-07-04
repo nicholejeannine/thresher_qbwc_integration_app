a0458b30581b5a95502618f51d2d7ea4847df340
nicholejeannine <nklerk@alopias.com>
2017-07-03 23:23:20 -0700

    Update project to also parse

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -2,15 +2,12 @@ class Customer < ApplicationRecord
 	self.abstract_class = true
 	
 	def self.parse_customer_response(qb)
-		unless qb.has_key?('data_ext_ret')
-			qb.merge!("data_ext_ret" => [{"data_ext_name" => "Site Contact", "data_ext_value" => ""}, {"data_ext_name" => "Site Email", "data_ext_value" => ""}, {"data_ext_name" => "Site Phone", "data_ext_value" => ""}])
-		end
 		if qb['sublevel'] == 0 && !qb['name'].upcase.start_with?('P-')
 			Client.parse_qb_response(qb)
 		elsif qb['sublevel'] > 0 && !qb['name'].upcase.start_with?('P-')
 			Job.parse_qb_response(qb)
 		elsif qb['sublevel'] > 0 && qb['name'].upcase.start_with?('P-')
-			Project.new_project(qb)
+			Project.parse_project(qb)
 		end
 	end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,23 +1,31 @@
 class Project < ApplicationRecord
   include QuickbooksQueryable
+  def self.qb_id
+    "list_id"
+  end
+  
   before_save :titleize_job_status
 
-  def self.new_project(qb)
+  def self.parse_project(qb)
     begin
       # First try to find by list id
-    p =   Project.where(:list_id => qb['list_id'])&.first
+    p =   Project.where("list_id" => qb['list_id'])&.first
     # Then try to find by full name
-    p ||= Project.where(:full_name => qb['full_name'])&.first
+    p ||= Project.where("full_name" => qb['full_name'])&.first
     # If it's not found, write to error log. If it's found, let it update.
     if p.nil?
-      QbwcError.create!(:worker_class => "Project::No Match Found", :model_id => qb['list_id'], :error_message => "No project in the portal matches customer #{qb['full_name']} in Quickbooks")
+      raise StandardError, "Project Not Found"
     else
-      p.parse_hash(qb.to_hash)
-      p.save
+      hash =  self.serialize_query_response(qb)
+      p.update!(hash)
     end
-    rescue Exception => e
-      QbwcError.create(:worker_class => "Project", :model_id => "#{self.id}", :error_message => "#{e}")
+    rescue StandardError => e
+      QbwcError.create(:worker_class => "Project", :model_id => "#{qb["full_name"]}", :error_message => "#{e}")
     end
+  end
+  
+  def self.attributes
+    {"list_id" => nil, "time_created" => nil, "time_modified" => nil, "edit_sequence" => nil, "name" => nil, "full_name" => nil, "is_active" => true, "parent_id" => nil, "sublevel" => nil, "company_name" => nil, "salutation" => nil, "first_name" => nil, "middle_name" => nil, "last_name" => nil, "job_title" => nil, "bill_addr1" => nil, "bill_addr2" => nil, "bill_addr3" => nil, "bill_addr4" => nil, "bill_addr5" => nil, "bill_city" => nil, "bill_state" => nil, "bill_postal_code" => nil, "bill_country" => nil, "bill_note" => nil, "ship_addr1" => nil, "ship_addr2" => nil, "ship_addr3" => nil, "ship_addr4" => nil, "ship_addr5" => nil, "ship_city" => nil, "ship_state" => nil, "ship_postal_code" => nil, "ship_country" => nil, "ship_note" => nil, "phone" => nil, "alt_phone" => nil, "fax" => nil, "email" => nil, "cc" => nil, "contact" => nil, "alt_contact" => nil, "customer_type" => nil, "terms" => nil, "sales_rep" => nil, "balance" => 0, "total_balance" => 0, "sales_tax_code" => nil, "item_sales_tax" => nil, "account_number" => nil, "job_status" => nil, "job_start_date" => nil, "job_projected_end_date" => nil, "job_end_date" => nil, "job_desc" => nil, "job_type" => nil, "preferred_delivery_method" => nil, "site_contact" => nil, "site_email" => nil, "site_phone" => nil}
   end
   
   private
