58a45dbfba1b72e7b1aa22f2d84d610fb001c9ac
nicholejeannine <nklerk@alopias.com>
2017-07-07 12:15:41 -0700

    This works a lot better

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,8 +1,11 @@
 class Client < ApplicationRecord
   include QuickbooksQueryable
+  
   def self.qb_id
     "list_id"
   end
+
+  QB_KEYS = %w(list_id time_created time_modified edit_sequence full_name is_active company_name salutation first_name middle_name last_name job_title bill_address ship_address phone alt_phone fax email cc contact alt_contact customer_type_ref terms_ref sales_rep_ref balance total_balance sales_tax_code_ref item_sales_tax_ref account_number job_type_ref preferred_delivery_method data_ext_ret)
   
   def self.attributes
     {"list_id" =>nil, "time_created" =>nil, "time_modified" =>nil, "edit_sequence" =>nil, "full_name" =>nil, "is_active" =>true, "company_name" =>nil, "salutation" =>nil, "first_name" =>nil, "middle_name" =>nil, "last_name" =>nil, "job_title" =>nil, "bill_addr1" =>nil, "bill_addr2" =>nil, "bill_addr3" =>nil, "bill_addr4" =>nil, "bill_addr5" =>nil, "bill_city" =>nil, "bill_state" =>nil, "bill_postal_code" =>nil, "bill_country" =>nil, "bill_note" =>nil, "ship_addr1" =>nil, "ship_addr2" =>nil, "ship_addr3" =>nil, "ship_addr4" =>nil, "ship_addr5" =>nil, "ship_city" =>nil, "ship_state" =>nil, "ship_postal_code" =>nil, "ship_country" =>nil, "ship_note" =>nil, "phone" =>nil, "alt_phone" =>nil, "fax" =>nil, "email" =>nil, "cc" =>nil, "contact" =>nil, "alt_contact" =>nil, "customer_type" =>nil, "terms" =>nil, "sales_rep" =>nil, "balance" =>0, "total_balance" =>0, "sales_tax_code" =>nil, "item_sales_tax" =>nil, "account_number" =>nil, "job_type" =>nil, "preferred_delivery_method" =>nil, "site_contact" =>nil, "site_email" =>nil, "site_phone" =>nil}
--- app/models/concerns/quickbooks_queryable.rb
+++ app/models/concerns/quickbooks_queryable.rb
@@ -4,85 +4,45 @@ module QuickbooksQueryable
   include QuickbooksLineItemUtils
   
   # Handle reference types - save only the value labeled "full name"
-  def handle_ref_type(key, value)
-    begin
-      if key.match(/customer_ref|vendor_ref|parent_ref/)
-        update_attribute("#{key.remove(/ref$/).concat('id')}", value['list_id'])
-        update_attribute("#{key.remove(/ref$/).concat('type')}", name_type(value['full_name'])) if key == 'customer_ref'
-      else
-        update_attribute("#{key.remove(/_ref$/)}", value['full_name'])
-      end
-    rescue Exception => e
-      QbwcError.create(:worker_class => "#{self.class.name}", :model_id => "#{self.id}", :error_message => "Error when attempting operation #{key}=#{value}: #{e}")
-    end
-  end
-  
-  # Try to update the attribute, only if the column named exists in the database table.
-  # TODO: Throw an error when this is called and returns false due to self.respond_to? == false
-  def update_attribute(column_name, new_value)
-    begin
-      if self.respond_to?("#{column_name}=")
-        self.send("#{column_name}=", new_value)
-      end
-    rescue Exception => e
-      QbwcError.create(:worker_class => "#{self.class.name}", :model_id => "#{self.id}", :error_message => "Error when attempting operation #{column_name}=#{new_value}: #{e}")
-    end
-  end
-  
-  def handle_custom_type(value)
-    value&.each do |arr|
-      if arr['data_ext_name'] == 'Site Contact'
-        update_attribute("site_contact", arr['data_ext_value'])
-      elsif arr['data_ext_name'] == 'Site Email'
-        update_attribute("site_email", arr['data_ext_value'])
-      elsif arr['data_ext_name'] == 'Site Phone'
-        update_attribute("site_phone", arr['data_ext_value'])
-      end
-    end
-  end
-  
-  def handle_address(key, value)
-    prefix = key.remove(/address/)
-    value&.each do |k, v|
-      unless ignored_type?(k)
-        update_attribute("#{prefix}#{k}", v)
-      end
-    end
-  end
-  
-  # Takes a quickbooks hash and deals with each key/value pair according to its xml type.
-  # TODO: shorten this - maybe pull out contacts, and parse the remainder, as two separate method calls?
-  def parse_hash(qb)
-    begin
-      qb&.each do |key, value|
-        next if ignored_type?(key) # skip ignored items.
-        if line_item_type?(key)
-          l = self.line_klass.parse_qb_response(value)
-          l.update_attribute!("#{self.class.name}_id=", self.id)
-        elsif linked_txn?(key)
-          value.each{|hash|parse_linked_txn(hash)}
-        elsif address?(key)
-          handle_address(key, value)
-        elsif ref_type?(key)
-          handle_ref_type(key, value)
-        elsif custom_type?(key)
-          handle_custom_type(value)
-        else update_attribute(key, value)
-        end
-      end
-    rescue StandardError => e
-      QbwcError.create(:worker_class => "#{self.class.name}", :model_id => "#{self.id}", :error_message => "QB hash parsing error: #{e}")
-    end
-  end
+  # def handle_ref_type(key, value)
+  #   begin
+  #     if key.match(/customer_ref|vendor_ref|parent_ref/)
+  #       update_attribute("#{key.remove(/ref$/).concat('id')}", value['list_id'])
+  #       update_attribute("#{key.remove(/ref$/).concat('type')}", name_type(value['full_name'])) if key == 'customer_ref'
+  #     else
+  #       update_attribute("#{key.remove(/_ref$/)}", value['full_name'])
+  #     end
+  #   rescue Exception => e
+  #     QbwcError.create(:worker_class => "#{self.class.name}", :model_id => "#{self.id}", :error_message => "Error when attempting operation #{key}=#{value}: #{e}")
+  #   end
+  # end
   
   included do
     def self.parse_qb_response(qb)
       begin
-        qb_id = self.qb_id
-        qb_value = qb[qb_id]
-        c = self.find_or_initialize_by(qb_id => qb_value)
-        c.parse_hash(Qbxml::Hash.from_hash(qb))
-        c.save
+        hash = Qbxml::Hash.from_hash(qb).extract!(*self::QB_KEYS)
+        data = Hash.new(nil)
+        qb_value = qb[self.qb_id]
+        c = self.find_or_initialize_by(self.qb_id => qb_value)
+        hash.each do |key, value|
+          if key.match(/_ref$/)
+            name = key.remove(/_ref$/)
+            full_name = value['full_name']
+            data.store(name, full_name)
+          elsif key.match(/_address$/)
+            name = key.remove(/_address$/)
+            value.each do |k,v|
+              data.store("#{name}_#{k}", v) unless k == 'xml_attributes'
+            end
+          elsif key.match(/data_ext_ret/)
+            value&.each do |arr|
+              name = arr['data_ext_name'].remove(" ").underscore
+              data.store(name, arr["data_ext_value"])
+            end
+          end
+        end
+        hash.select!{|k|k.in?(self.attributes.keys)}.merge!(data)
+        c.update(c.attributes.merge!(hash))
       rescue StandardError => e
         QbwcError.create(:worker_class => "#{self.class.name}", :model_id => "#{qb_value}", :error_message => "Error parsing response: #{e}")
       end
