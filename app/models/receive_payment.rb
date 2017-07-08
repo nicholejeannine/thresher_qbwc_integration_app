c15c2f0af542383a20b04c25c5d753037bfa18d5
nicholejeannine <nklerk@alopias.com>
2017-07-08 03:36:25 -0700

    Correction to typo

--- app/models/receive_payment.rb
+++ app/models/receive_payment.rb
@@ -5,7 +5,7 @@ class ReceivePayment < ApplicationRecord
   def self.qb_id
     "txn_id"
   end
-  
+  QB_KEYS = %w(txn_id time_created time_modified edit_sequence txn_number customer_ref ar_account txn_date ref_number total_amount payment_method memo deposit_to_account unused_payment unused_credits URL)
   def self.attributes
     {"txn_id" => nil, "time_created" => nil, "time_modified" => nil, "edit_sequence" => nil, "txn_number" => nil, "customer_id" => nil, "customer_type" => nil, "invoice_id" => nil, "ar_account" => nil, "txn_date" => nil, "ref_number" => nil, "total_amount" => nil, "payment_method" => nil, "memo" => nil, "deposit_to_account" => nil, "unused_payment" => nil, "unused_credits" => nil}
   end
