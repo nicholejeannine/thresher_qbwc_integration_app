cd6828d72dcf24b2f087e65cc1f83426350bd106
nicholejeannine <nklerk@alopias.com>
2017-07-08 03:34:01 -0700

    QB keys for invoice and invoice lines

--- app/models/invoice_line.rb
+++ app/models/invoice_line.rb
@@ -2,6 +2,8 @@ class InvoiceLine < ApplicationRecord
   include QuickbooksQueryable
   belongs_to :invoice
 
+  QB_KEYS = %w(txn_line_id item_ref desc quantity unit_of_measure override_uom_set_ref rate rate_percent amount inventory_site_ref inventory_site_location_ref serial_number lot_number service_date sales_tax_code_ref)
+  
   def self.qb_id
     "txn_line_id"
   end
