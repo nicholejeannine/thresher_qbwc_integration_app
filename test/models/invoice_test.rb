a67d8cd25e2c71bc85677af8c870ef84421f596e
nicholejeannine <nklerk@alopias.com>
2017-07-08 19:51:38 -0700

    Allow invoice line items and add test to make ure they're getting added

--- test/models/invoice_test.rb
+++ test/models/invoice_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class InvoiceTest < ActiveSupport::TestCase
   test "can parse a simple hash" do
     assert_equal(1, Invoice.count, "should equal one")
+    assert_equal(7, InvoiceLine.count, "should parse lines")
+
   end
   
   
