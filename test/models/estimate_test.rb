c63a5192fa5278ca914cc7e9d1b38286b96666fb
nicholejeannine <nklerk@alopias.com>
2017-07-10 22:17:18 -0700

    Finish estimate test

--- test/models/estimate_test.rb
+++ test/models/estimate_test.rb
@@ -6,11 +6,36 @@ class EstimateTest < ActiveSupport::TestCase
     assert_equal(1, Estimate.count, "should equal one")
     assert_equal(29, EstimateLine.count, "should parse estimate lines")
   end
-  #
-  # test "can parse customer ref" do
-  #   assert_equal("", Estimate.first.customer)
-  # end
-  #
+
+  test "can save billing address" do
+    assert_equal("Box HQ", Estimate.first.bill_addr1, "Bill Address should parse")
+  end
+
+  test "can save ship address" do
+    assert_equal("900 Jefferson Road", Estimate.first.ship_addr1, "Ship Address should parse")
+  end
+
+  test "handles ref types" do
+    assert_equal("ARJ", Estimate.first.sales_rep)
+    assert_equal("Custom Estimate", Estimate.first.template)
+    assert_equal("SM/CA", Estimate.first.item_sales_tax)
+    assert_equal("Tax", Estimate.first.customer_sales_tax_code)
+  end
+  
+  test "updates existing hash" do
+    hash = {"txn_id" => "53D7E5-1497890620", "txn_date" => "2018-06-19", "ref_number" => "10634a",  "is_active" => false, "due_date" => "2019-06-19", "sales_tax_percentage" => 19.0, "sales_tax_total" => 100.00, "total_amount" => 100.00, "is_to_be_emailed" => true, "customer_sales_tax_code_ref" => {"xml_attributes" => {}, "list_id" => "10000-1009221895", "full_name" => "AAA"}}
+    Estimate.parse_qb_response(hash)
+    assert_equal(1, Estimate.count)
+    assert_equal("2018-06-19", Estimate.first.txn_date.iso8601)
+    assert_equal("10634a", Estimate.first.ref_number)
+    assert_equal(false, Estimate.first.is_active)
+    assert_equal("2019-06-19", Estimate.first.due_date.iso8601)
+    assert_equal(19.0, Estimate.first.sales_tax_percentage)
+    assert_equal(100.00, Estimate.first.sales_tax_total.to_f)
+    assert_equal(true, Estimate.first.is_to_be_emailed)
+    assert_equal("AAA", Estimate.first.customer_sales_tax_code)
+  end
+  
   def setup
     Job.destroy_all
     Job.create!({"id" => 2, "list_id" => "80000F95-1427944431", "full_name" => "Box:Construction:Redwood City"})
