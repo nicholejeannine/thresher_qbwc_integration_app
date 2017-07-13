67a9cd5bed9543cc6ba1b734ab43140c241c012e
nicholejeannine <nklerk@alopias.com>
2017-07-12 19:32:07 -0700

    Re-reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,8 +2,8 @@
   QBWC.clear_jobs
   QBWC.add_job(:list, false, '', ListWorker)
   QBWC.add_job(:txn, false, '', TransactionQueryWorker)
-  QBWC.add_job(:initial, true, '', InitialWorker)
-  QBWC.add_job(:query, false, '', QueryWorker)
+  QBWC.add_job(:initial, false, '', InitialWorker)
+  QBWC.add_job(:query, true, '', QueryWorker)
   QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
   QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
   
