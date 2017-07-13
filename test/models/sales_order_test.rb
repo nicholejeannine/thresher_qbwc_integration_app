3c4bc7304e020a13e5f08240b779ae2da93f283b
nicholejeannine <nklerk@alopias.com>
2017-07-12 18:22:27 -0700

    Update SO tests to pass

--- test/models/sales_order_test.rb
+++ test/models/sales_order_test.rb
@@ -8,30 +8,37 @@ class SalesOrderTest < ActiveSupport::TestCase
   end
 
   test "can save billing address" do
-    assert_equal("Box HQ", SalesOrder.first.bill_addr1, "Bill Address should parse")
+    assert_equal("Square, Inc.", SalesOrder.first.bill_addr1, "Bill Address should parse")
   end
 
   test "can save ship address" do
-    assert_equal("900 Jefferson Road", SalesOrder.first.ship_addr1, "Ship Address should parse")
+    assert_equal("1455 Market St. 6th floor", SalesOrder.first.ship_addr3, "Ship Address should parse")
   end
 
   test "handles ref types" do
+    assert_equal("Sales Order with Rep", SalesOrder.first.template)
     assert_equal("ARJ", SalesOrder.first.sales_rep)
-    assert_equal("Custom SalesOrder", SalesOrder.first.template)
-    assert_equal("SM/CA", SalesOrder.first.item_sales_tax)
+    assert_equal("SF/CA", SalesOrder.first.item_sales_tax)
     assert_equal("Tax", SalesOrder.first.customer_sales_tax_code)
   end
 
   test "updates existing hash" do
-    hash = {"txn_id" => "53D7E5-1497890620", "txn_date" => "2018-06-19", "ref_number" => "10634a",  "is_active" => false, "due_date" => "2019-06-19", "sales_tax_percentage" => 19.0, "sales_tax_total" => 100.00, "total_amount" => 100.00, "is_to_be_emailed" => true, "customer_sales_tax_code_ref" => {"xml_attributes" => {}, "list_id" => "10000-1009221895", "full_name" => "AAA"}}
+    hash = {"txn_id" => "53DFA4-1498058332","txn_number" => 19, "txn_date" => "2018-06-19", "ref_number" => "10634a", "po_number" => "Ryan", "due_date" => "2019-06-19","ship_date" => "2011-01-01", "sales_tax_percentage" => 19.0, "sales_tax_total" => 100.00, "total_amount" => 101.00, "is_manually_closed" => true, "is_fully_invoiced" => true, "memo" => "Estimate 10644a:", "is_to_be_printed" => false, "is_to_be_emailed" => true, "customer_sales_tax_code_ref" => {"xml_attributes" => {}, "list_id" => "10000-1009221895", "full_name" => "AAA"}}
     SalesOrder.parse_qb_response(hash)
     assert_equal(1, SalesOrder.count)
+    assert_equal(19, SalesOrder.first.txn_number)
     assert_equal("2018-06-19", SalesOrder.first.txn_date.iso8601)
     assert_equal("10634a", SalesOrder.first.ref_number)
-    assert_equal(false, SalesOrder.first.is_active)
+    assert_equal("Ryan", SalesOrder.first.po_number)
     assert_equal("2019-06-19", SalesOrder.first.due_date.iso8601)
+    assert_equal("2011-01-01", SalesOrder.first.ship_date.iso8601)
     assert_equal(19.0, SalesOrder.first.sales_tax_percentage)
     assert_equal(100.00, SalesOrder.first.sales_tax_total.to_f)
+    assert_equal(101.00, SalesOrder.first.total_amount.to_f)
+    assert_equal(true, SalesOrder.first.is_manually_closed)
+    assert_equal(true, SalesOrder.first.is_fully_invoiced)
+    assert_equal("Estimate 10644a:", SalesOrder.first.memo)
+    assert_equal(false, SalesOrder.first.is_to_be_printed)
     assert_equal(true, SalesOrder.first.is_to_be_emailed)
     assert_equal("AAA", SalesOrder.first.customer_sales_tax_code)
   end
