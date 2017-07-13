27892927fb42bd823c8bbc34fa6df04a77ed1a88
nicholejeannine <nklerk@alopias.com>
2017-07-13 00:31:52 -0700

    Get rid of old migrations

--- db/migrate/20170712220110_create_invoice_payment_join_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateInvoicePaymentJoinTable < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :invoices, :receive_payments do |t|
-      # t.index [:invoice_id, :receive_payment_id]
-      t.index [:receive_payment_id, :invoice_id], unique: true, name: 'invoice_payment'
-    end
-  end
-end
