0483c7701b6c845d0a2eda831cebe6aa99b98f55
nicholejeannine <nklerk@alopias.com>
2017-07-12 21:01:30 -0700

    Invoices save sales order id now hooray

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -4,18 +4,18 @@ class Invoice < ApplicationRecord
   has_many :receive_payments, through: :invoices_receive_payments
   before_save :parse_memo
 
-  def self.parse_linked_txn(ret, id)
+  def self.parse_linked_txn(ret, invoice_id)
     begin
       ret.each do |hash|
         if hash["txn_type"] == "SalesOrder"
-        i = Invoice.find(id)
+        i = Invoice.find(invoice_id)
         txn_id = hash["txn_id"]
         sales_order_id = SalesOrder.find_by("txn_id" => txn_id)&.id
-        i.send("sales_order_id=", "#{sales_order_id}") unless sales_order_id.nil?
+        i.update!({"sales_order_id" => sales_order_id}) unless sales_order_id.nil?
         end
       end
     rescue => e
-      QbwcError.create(:worker_class => "Parse Linked Txn Invoice", :model_id => "#{id}", :error_message => "Error parsing Invoice txn to assign sales_order_id: #{e}")
+      QbwcError.create(:worker_class => "Parse Linked Txn Invoice", :model_id => "#{invoice_id}", :error_message => "Error parsing Invoice txn to assign sales_order_id: #{e}")
     end
   end
 
