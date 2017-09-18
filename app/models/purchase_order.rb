6b3a8b42e7127d0bdc28d887f2500c3bdee73780
nicholejeannine <nklerk@alopias.com>
2017-09-18 13:41:35 -0700

    PO parses estimate and sales order (but it's UGLY)

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -5,15 +5,17 @@ class PurchaseOrder < ApplicationRecord
 
   def parse_memo
     begin
-       so = /[sSalesOrder ](\d+)/
-       estimate = /[estimate (\d+)]/
-       if memo&.downcase&.match(so)
-        ref = $1
-        self.sales_order_id = SalesOrder.where(:ref_number => ref).first&.id
-      end
-      if memo&.downcase&.match(estimate)
-        e_ref = $1
-        self.estimate_id = Estimate.where(:ref_number => e_ref)&.first&.id
+      references = memo.downcase.split(":")
+      references.each do |slice|
+        if slice.strip.match(/sales order|salesorder/)
+          ref = slice.match(/(\d+)/)
+          ref = $1
+          self.sales_order_id = SalesOrder.where(:ref_number => ref).first&.id
+        elsif slice.strip.match(/estimate/)
+          ref = slice.match(/(\d+)/)
+          ref = $1
+          self.estimate_id = Estimate.where(:ref_number => ref)&.first&.id
+        end
       end
     rescue => e
       QbwcError.create(:worker_class => self.class.name, :model_id => self.id, :error_message => "Error parsing Purchase Order memo to assign estimate_id or sales order id: #{e}")
