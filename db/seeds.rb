5bac748b11c72600804f9093d0386e3df979f8c9
nicholejeannine <nklerk@alopias.com>
2017-09-18 11:33:57 -0700

    Modify seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
 # clear quickbooks jobs
   QBWC.clear_jobs
   QBWC.add_job(:initial, true, '', InitialWorker)
-  QBWC.add_job(:query, false, '', QueryWorker)
-  QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
-  QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
+  # QBWC.add_job(:query, false, '', QueryWorker)
+  # QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
+  # QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
   
