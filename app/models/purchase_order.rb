47f6928016b170d7b9f17eb8e3368f20985cfd67
nicholejeannine <nklerk@alopias.com>
2017-09-18 16:17:47 -0700

    Try fix error when PO memo field is blank

--- app/models/purchase_order.rb
+++ app/models/purchase_order.rb
@@ -5,16 +5,18 @@ class PurchaseOrder < ApplicationRecord
 
   def parse_memo
     begin
-      references = memo.downcase.split(":")
-      references.each do |slice|
-        if slice.strip.match(/sales order|salesorder/)
-          ref = slice.match(/(\d+)/)
-          ref = $1
-          self.sales_order_id = SalesOrder.where(:ref_number => ref)&.first&.id
-        elsif slice.strip.match(/estimate/)
-          ref = slice.match(/(\d+)/)
-          ref = $1
-          self.estimate_id = Estimate.where(:ref_number => ref)&.first&.id
+      unless memo.blank?
+        references = memo.downcase.split(":")
+        references.each do |slice|
+          if slice.strip.match(/sales order|salesorder/)
+            ref = slice.match(/(\d+)/)
+            ref = $1
+            self.sales_order_id = SalesOrder.where(:ref_number => ref)&.first&.id
+          elsif slice.strip.match(/estimate/)
+            ref = slice.match(/(\d+)/)
+            ref = $1
+            self.estimate_id = Estimate.where(:ref_number => ref)&.first&.id
+          end
         end
       end
     rescue => e
