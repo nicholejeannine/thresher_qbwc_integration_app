01d3b535caca12e361ed0785106b819f522162d4
nicholejeannine <nklerk@alopias.com>
2017-12-09 19:45:28 -0800

    Remove request entirely from worker, try seeding multiple add requests in same job

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -1,9 +1,5 @@
 class TimeTrackingAddWorker <  QBWC::Worker
 
-  # def requests(job, session, data)
-  #    [{:time_tracking_add_rq => {:time_tracking_add => {:txn_date => "2017-12-09", :entity_ref => {:full_name => "Klerk, Nichole J"}, :customer_ref => {:full_name => "TCP:IT:P-903"}, :item_service_ref => {:full_name => "Video:0100"}, :duration => "PT8H0M0S", :payroll_item_wage_ref => {:full_name => "Hourly Level 1"}, :billable_status => "Billable"}}}]
-  # end
-
   def handle_response(r, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     QBWC.delete_job(job)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,59 @@
 # clear quickbooks jobs
+  requests = [{:time_tracking_add_rq =>
+                   {:time_tracking_add =>
+                        {:txn_date => "2017-12-04",
+                         :entity_ref => {:full_name => "Klerk, Nichole J"},
+                         :customer_ref => {:full_name => "TCP:IT:P-903"},
+                         :item_service_ref => {:full_name => "Video:0100"},
+                         :duration => "PT8H0M0S",
+                         :payroll_item_wage_ref => {:full_name => "Hourly Level 1"},                                       :billable_status => "Billable"}
+                   },
+              },
+              {:time_tracking_add_rq =>
+                   {:time_tracking_add =>
+                        {:txn_date => "2017-12-05",
+                         :entity_ref => {:full_name => "Klerk, Nichole J"},
+                         :customer_ref => {:full_name => "TCP:IT:P-903"},
+                         :item_service_ref => {:full_name => "Video:0100"},
+                         :duration => "PT8H0M0S",
+                         :payroll_item_wage_ref => {:full_name => "Hourly Level 1"},                                       :billable_status => "Billable"}
+                   },
+              },
+              {:time_tracking_add_rq =>
+                   {:time_tracking_add =>
+                        {:txn_date => "2017-12-06",
+                         :entity_ref => {:full_name => "Klerk, Nichole J"},
+                         :customer_ref => {:full_name => "TCP:IT:P-903"},
+                         :item_service_ref => {:full_name => "Video:0100"},
+                         :duration => "PT8H0M0S",
+                         :payroll_item_wage_ref => {:full_name => "Hourly Level 1"},                                       :billable_status => "Billable"}
+                   },
+              },
+              {:time_tracking_add_rq =>
+                   {:time_tracking_add =>
+                        {:txn_date => "2017-12-07",
+                         :entity_ref => {:full_name => "Klerk, Nichole J"},
+                         :customer_ref => {:full_name => "TCP:IT:P-903"},
+                         :item_service_ref => {:full_name => "Video:0100"},
+                         :duration => "PT8H0M0S",
+                         :payroll_item_wage_ref => {:full_name => "Hourly Level 1"},                                       :billable_status => "Billable"}
+                   },
+              },
+              {:time_tracking_add_rq =>
+                   {:time_tracking_add =>
+                        {:txn_date => "2017-12-09",
+                         :entity_ref => {:full_name => "Klerk, Nichole J"},
+                         :customer_ref => {:full_name => "TCP:IT:P-903"},
+                         :item_service_ref => {:full_name => "Video:0100"},
+                         :duration => "PT8H0M0S",
+                         :payroll_item_wage_ref => {:full_name => "Hourly Level 1"},                                       :billable_status => "Billable"}
+                   },
+              }]
+
   QBWC.clear_jobs
   QBWC.add_job(:initial, true, '', InitialWorker)
   QBWC.add_job(:query, false, '', QueryWorker)
   QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
   QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
-  QBWC.add_job(:add_time, false, '', TimeTrackingAddWorker, [{:time_tracking_add_rq => {:time_tracking_add => {:txn_date => "2017-12-09", :entity_ref => {:full_name => "Klerk, Nichole J"}, :customer_ref => {:full_name => "TCP:IT:P-903"}, :item_service_ref => {:full_name => "Video:0100"}, :duration => "PT8H0M0S", :payroll_item_wage_ref => {:full_name => "Hourly Level 1"}, :billable_status => "Billable"}}}])
+  QBWC.add_job(:add_time, false, '', TimeTrackingAddWorker, requests)
 
