052c886f51cdda1d908ba9764a9948330b2ad154
nicholejeannine <nklerk@alopias.com>
2017-12-09 20:04:48 -0800

    Fix handle_response to prevent error

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -3,6 +3,8 @@ class TimeTrackingAddWorker <  QBWC::Worker
   def handle_response(r, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     # QBWC.delete_job(job)
-    Rails.logger.log(r)
+    r['time_tracking_ret']&.each do |response|
+      QbwcHistory.create(:worker_class => "AddTime", :error_message => response)
+    end
   end
 end
