a60e7d0bd9aac9e9434a7c1084fd2e857cd8fba8
nicholejeannine <nklerk@alopias.com>
2017-12-09 20:05:58 -0800

    Use correct model

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -4,7 +4,7 @@ class TimeTrackingAddWorker <  QBWC::Worker
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     # QBWC.delete_job(job)
     r['time_tracking_ret']&.each do |response|
-      QbwcHistory.create(:worker_class => "AddTime", :error_message => response)
+      QbwcError.create(:worker_class => "AddTime", :error_message => response)
     end
   end
 end
