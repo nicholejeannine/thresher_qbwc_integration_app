bf5df73df31bbecf123af11710322dd7570d2d31
nicholejeannine <nklerk@alopias.com>
2017-12-09 19:20:33 -0800

    Try next step - comment out the 'requests' part of TimeTrackingAddWorker, and try to pass in the request as an array inside the db/seeds.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,5 @@
   QBWC.add_job(:query, false, '', QueryWorker)
   QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
   QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
-  
+  QBWC.add_job(:add_time, false, '', TimeTrackingAddWorker, [{:time_tracking_add_rq => {:time_tracking_add => {:txn_date => "2017-12-09", :entity_ref => {:full_name => "Klerk, Nichole J"}, :customer_ref => {:full_name => "TCP:IT:P-903"}, :item_service_ref => {:full_name => "Video:0100"}, :duration => "PT8H0M0S", :payroll_item_wage_ref => {:full_name => "Hourly Level 1"}, :billable_status => "Billable"}}}])
+
