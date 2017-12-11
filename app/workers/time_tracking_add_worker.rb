cd9f8b7b793638c31d9fd31e78d6ff7263ca8bf6
nicholejeannine <nklerk@alopias.com>
2017-12-11 09:50:37 -0800

    Revert TimeTrackingAddWorker

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -1,7 +1,10 @@
 class TimeTrackingAddWorker <  QBWC::Worker
 
   def handle_response(r, session, job, request, data)
-
+    # handle_response will get customers in groups of 100. When this is 0, we're done.
+    # QBWC.delete_job(job)
+    r['time_tracking_ret']&.each do |response|
+      QbwcError.create(:worker_class => "AddTime", :error_message => response)
+    end
   end
-
 end
