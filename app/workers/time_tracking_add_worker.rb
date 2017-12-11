d07ea3dff5f2cfa5e51293ca798c60800779763b
nicholejeannine <nklerk@alopias.com>
2017-12-11 09:47:55 -0800

    Test

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -1,11 +1,7 @@
 class TimeTrackingAddWorker <  QBWC::Worker
 
   def handle_response(r, session, job, request, data)
-    # handle_response will get customers in groups of 100. When this is 0, we're done.
-    Rails.logger.warn("SESSION: #{session}")
-    Rails.logger.warn("JOB: #{job}")
-    Rails.logger.warn("REQUEST: #{request}")
-    Rails.logger.warn("DATA: #{data}")
+
   end
 
 end
