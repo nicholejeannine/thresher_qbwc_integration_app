6b5e0c7a78fbb795750767fdb35a42950d640899
nicholejeannine <nklerk@alopias.com>
2017-12-11 10:16:46 -0800

    TimeTrackingAdd - successful entries in TimeTrackings table?

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -2,22 +2,25 @@ class TimeTrackingAddWorker <  QBWC::Worker
 
   def handle_response(r, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
-    complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
-    if complete
-      Rails.logger.warn("\n\n\n")
-      Rails.logger.warn("SESSION: #{session}")
-      Rails.logger.warn("\n\n\n")
-      Rails.logger.warn("JOB: #{job}")
-      Rails.logger.warn("\n\n\n")
-      Rails.logger.warn("REQUEST: #{request}")
-      Rails.logger.warn("\n\n\n")
-      Rails.logger.warn("DATA: #{data}")
-    else
-      QbwcError.create(:worker_class => "AddTime Count", :error_message => "#{r['xml_attributes']['iteratorRemainingCount']} remain")
-    end
+    # complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
+    # if complete
+    #   Rails.logger.warn("\n\n\n")
+    #   Rails.logger.warn("SESSION: #{session}")
+    #   Rails.logger.warn("\n\n\n")
+    #   Rails.logger.warn("JOB: #{job}")
+    #   Rails.logger.warn("\n\n\n")
+    #   Rails.logger.warn("REQUEST: #{request}")
+    #   Rails.logger.warn("\n\n\n")
+    #   Rails.logger.warn("DATA: #{data}")
+    # else
+    #   QbwcError.create(:worker_class => "AddTime Count", :error_message => "#{r['xml_attributes']['iteratorRemainingCount']} remain")
+    # end
     # QBWC.delete_job(job)
-    r['time_tracking_ret']&.each do |response|
-      QbwcError.create(:worker_class => "AddTime", :error_message => response)
+    complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
+    begin
+      r['time_tracking_ret']&.each{|qb|TimeTracking.parse_qb_response(qb)}
+    rescue StandardError => e
+      QbwcError.create(:worker_class => self.class.name, :error_message => e)
     end
   end
 end
