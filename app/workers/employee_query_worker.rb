f98d8e1ebd02d6c2442ac065716a3f0573f72c5b
nicholejeannine <nklerk@alopias.com>
2017-12-11 11:40:37 -0800

    Remove response for now just to see the response returned in the log?

--- app/workers/employee_query_worker.rb
+++ app/workers/employee_query_worker.rb
@@ -15,11 +15,5 @@ class EmployeeQueryWorker < QBWC::Worker
   def handle_response(r, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
-    begin
-      r['employee_ret']&.each{|qb|Employee.parse_qb_response(qb)}
-    rescue StandardError => e
-      QbwcError.create(:worker_class => self.class.name, :error_message => e)
-    end
-
   end
 end
