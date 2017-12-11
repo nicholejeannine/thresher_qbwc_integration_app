1f552438d3e3f42b63412a11d9cf9afffe589268
nicholejeannine <nklerk@alopias.com>
2017-12-11 10:52:10 -0800

    Add rack-cors, try configure

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,14 @@ module Railstest
 
     config.api_only = :true
 
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '65.19.134.198, 10.30.20.21, localhost, 127.0.0.1'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
     config.beginning_of_week = :sunday
     config.time_zone = 'Pacific Time (US & Canada)'
 
