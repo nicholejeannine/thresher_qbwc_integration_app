f8e626fe43cae5b1f388077d5a122ec3fee29f60
nicholejeannine <nklerk@alopias.com>
2017-12-11 11:34:22 -0800

    Add EmployeeQueryWorker, reseed

---
+++ app/workers/employee_query_worker.rb
@@ -0,0 +1,25 @@
+class EmployeeQueryWorker < ApplicationWorker
+
+  def requests(job, session, data)
+    [
+        {:employee_query_rq => {
+            :xml_attributes => { :requestID =>1, :iterator  => "Start" },
+            :max_returned => 100,
+            :active_status => "ActiveOnly",
+            :include_ret_element => ['ListID', 'Name', 'IsActive', 'BillingRateRef']
+        }
+        }]
+  end
+
+
+  def handle_response(r, session, job, request, data)
+    # handle_response will get customers in groups of 100. When this is 0, we're done.
+    complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
+    begin
+      r['employee_ret']&.each{|qb|Employee.parse_customer_response(qb)}
+    rescue StandardError => e
+      QbwcError.create(:worker_class => self.class.name, :error_message => e)
+    end
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,6 @@
   QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
   QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
   QBWC.add_job(:time_tracking_query, false, '', TimeTrackingQueryWorker)
-  QBWC.add_job(:add_time, true, '', TimeTrackingAddWorker, requests)
+  QBWC.add_job(:add_time, false, '', TimeTrackingAddWorker, requests)
+  QBWC.add_job(:employee_query, true, '', EmployeeQueryWorker)
 
