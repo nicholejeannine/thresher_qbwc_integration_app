7a479bcc3eae51f402e1f7565bd11ad81dbcf904
nicholejeannine <nklerk@alopias.com>
2017-12-11 11:07:21 -0800

    Test

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -2,25 +2,23 @@ class TimeTrackingAddWorker <  QBWC::Worker
 
   def handle_response(r, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
-    # complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
-    # if complete
-    #   Rails.logger.warn("\n\n\n")
-    #   Rails.logger.warn("SESSION: #{session}")
-    #   Rails.logger.warn("\n\n\n")
-    #   Rails.logger.warn("JOB: #{job}")
-    #   Rails.logger.warn("\n\n\n")
-    #   Rails.logger.warn("REQUEST: #{request}")
-    #   Rails.logger.warn("\n\n\n")
-    #   Rails.logger.warn("DATA: #{data}")
-    # else
-    #   QbwcError.create(:worker_class => "AddTime Count", :error_message => "#{r['xml_attributes']['iteratorRemainingCount']} remain")
-    # end
-    # QBWC.delete_job(job)
     complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
+    if complete
+      Rails.logger.warn("\n\n\n")
+      Rails.logger.warn("SESSION: #{session}")
+      Rails.logger.warn("\n\n\n")
+      Rails.logger.warn("JOB: #{job}")
+      Rails.logger.warn("\n\n\n")
+      Rails.logger.warn("REQUEST: #{request}")
+      Rails.logger.warn("\n\n\n")
+      Rails.logger.warn("DATA: #{data}")
+    else
+      QbwcError.create(:worker_class => "AddTime Count", :error_message => "#{r['xml_attributes']['iteratorRemainingCount']} remain")
+    end
     begin
       r['time_tracking_ret']&.each{|qb|TimeTracking.parse_qb_response(qb)}
     rescue StandardError => e
-      QbwcError.create(:worker_class => self.class.name, :error_message => e)
+      QbwcError.create(:worker_class => self.class.name, :error_message => "#{e.message}")
     end
   end
 end
