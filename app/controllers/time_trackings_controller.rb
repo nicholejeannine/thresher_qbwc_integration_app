5ced44ac1067b5a369330db51666acfa3778c2fa
nicholejeannine <nklerk@alopias.com>
2017-12-11 16:16:58 -0800

    Add route to add request to web connector for AddTimeTracking worker

---
+++ app/controllers/time_trackings_controller.rb
@@ -0,0 +1,32 @@
+class TimeTrackingsController < ApplicationController
+
+  # get '/time_trackings'
+  def index
+    @times = TimeTracking.all
+    render json: @times
+  end
+
+  # get '/time_trackings_add?employee_list_id=77777&txn_date=2017-11-11&duration=PT8H0M0S&customer_full_name=TCP:IT:P-903&notes=23004'
+  def add_request
+    new_request = build_request(params)
+    name = "AddTime_#{Time.now.to_i}"
+    @job = QBWC.add_job(name, true, '', TimeTrackingAddWorker, new_request)
+    if @job
+      render json: @job
+    end
+  end
+
+
+
+  def build_request(params)
+    {:time_tracking_add_rq => {:time_tracking_add => {:txn_date => params['txn_date'], :entity_ref => {:list_id => params['employee_list_id']}, :customer_ref => {:full_name => params['customer_full_name']}, :item_service_ref => {:full_name => "Video:0100"}, :duration => params['duration'], :class_ref => {:list_id => "200000-991719211"}, :payroll_item_wage_ref => {:full_name => "Hourly Level 1"}, :notes => params['notes'], :billable_status => "NotBillable"}}}
+  end
+
+
+
+  private
+  def time_tracking_params
+    params.require(:employee_list_id).permit(:txn_date, :customer_full_name, :duration, :notes)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Quickbooks stuff
   get 'qbwc/action' => 'qbwc#_generate_wsdl'
   get 'qbwc/qwc' => 'qbwc#qwc'
+  get 'time_trackings' => 'time_trackings#index'
+  get 'time_trackings_add' => 'time_trackings#add_request'
   wash_out :qbwc
 
   root to: 'home#index'
