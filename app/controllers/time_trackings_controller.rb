5f222441dd7308c7ea0123fe70ff49fb95f34682
nicholejeannine <nklerk@alopias.com>
2017-12-12 13:00:02 -0800

    Default value for item_service_ref

--- app/controllers/time_trackings_controller.rb
+++ app/controllers/time_trackings_controller.rb
@@ -19,14 +19,15 @@ class TimeTrackingsController < ApplicationController
 
 
   def build_request(params)
-    {:time_tracking_add_rq => {:time_tracking_add => {:txn_date => params['txn_date'], :entity_ref => {:list_id => params['employee_list_id']}, :customer_ref => {:full_name => params['customer_full_name']}, :item_service_ref => {:full_name => "Video:0100"}, :duration => params['duration'], :class_ref => {:list_id => "200000-991719211"}, :payroll_item_wage_ref => {:full_name => "Hourly Level 1"}, :notes => params['notes'], :billable_status => "NotBillable"}}}
+    item_service_ref = params['item_service_ref'] ||= "Video:0100"
+    {:time_tracking_add_rq => {:time_tracking_add => {:txn_date => params['txn_date'], :entity_ref => {:list_id => params['employee_list_id']}, :customer_ref => {:full_name => params['customer_full_name']}, :item_service_ref => {:full_name => item_service_ref}, :duration => params['duration'], :class_ref => {:list_id => "200000-991719211"}, :payroll_item_wage_ref => {:full_name => "Hourly Level 1"}, :notes => params['notes'], :billable_status => "NotBillable"}}}
   end
 
 
 
   private
   def time_tracking_params
-    params.require(:employee_list_id).permit(:txn_date, :customer_full_name, :duration, :notes)
+    params.require(:employee_list_id).permit(:txn_date, :customer_full_name, :item_service_ref, :duration, :notes)
   end
 
 end
