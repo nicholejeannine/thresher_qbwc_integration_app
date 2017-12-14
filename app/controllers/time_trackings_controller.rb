d145ae80932738064f8ad3dd8dbb2d5cab86d7f2
nicholejeannine <nklerk@alopias.com>
2017-12-13 22:03:20 -0800

    Try sending regular content

--- app/controllers/time_trackings_controller.rb
+++ app/controllers/time_trackings_controller.rb
@@ -8,8 +8,11 @@ class TimeTrackingsController < ApplicationController
 
   # post '/time_trackings
   def add_request
-    new_request = build_request(params)
-    name = "AddTime_#{Time.now.to_i}"
+    respond_to do |format|
+      format.json
+    end
+    # new_request = build_request(params)
+    # name = "AddTime_#{Time.now.to_i}"
     @job = QBWC.add_job(name, true, '', TimeTrackingAddWorker, new_request)
     if @job
       render json: params
