9d688731a402990d33ec42d7367d973df69eb9b7
nicholejeannine <nklerk@alopias.com>
2017-12-15 01:47:24 -0800

    IDE config fail

--- app/models/timecard_transaction.rb
+++ app/models/timecard_transaction.rb
@@ -2,11 +2,11 @@ class TimecardTransaction < ActiveRecord::Base
   self.primary_key= :id
   belongs_to :employee
   belongs_to :holiday, optional: true
-  belongs to :client
+  belongs_to :client
   belongs_to :job, optional: true
   belongs_to :project, optional: true
   belongs_to :ticket, optional: true
-  # grab all ids for timecards between a specified start and end date
+  # grab all timecards between a specified start and end date
   def self.between(start_date, end_date)
     includes(:employee, :holiday, :project, :ticket).where('`tc_date` >= ?', start_date).where('`tc_date` <= ?', end_date)
   end
