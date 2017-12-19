15f9f51072a3a1d561591eb97c4b471c3b703ac4
nicholejeannine <nklerk@alopias.com>
2017-12-18 23:21:19 -0800

    Add method and test for ticket service code

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -4,4 +4,11 @@ class Ticket < ActiveRecord::Base
   belongs_to :ticket_type, class_name: "TicketType", foreign_key: "ticket_type_id"
   belongs_to :ticket_subtype1, class_name: "TicketType", foreign_key: "ticket_subtype1_id"
   belongs_to :ticket_subtype2, class_name: "TicketType", foreign_key: "ticket_subtype2_id"
+
+  def service_code
+    one = self.ticket_type&.service_code || "0"
+    two = self.ticket_subtype1&.service_code || "0"
+    three = self.ticket_subtype2&.service_code || "0"
+    "0#{one}#{two}#{three}"
+  end
 end
