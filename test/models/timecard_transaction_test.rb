2bd70b8dcecc292b14b02c0cf5395946db35f345
nicholejeannine <nklerk@alopias.com>
2017-12-18 17:38:13 -0800

    Test lookup_customer_name method

--- test/models/timecard_transaction_test.rb
+++ test/models/timecard_transaction_test.rb
@@ -9,8 +9,26 @@ class TimecardTransactionTest < ActiveSupport::TestCase
     assert_equal(112, TicketType.count, "should equal 112 ticket types")
   end
 
-  def setup
 
+  test "between" do
+    assert_equal(363, TimecardTransaction.between("2017-12-03", "2017-12-09").count)
+    assert_equal(0, TimecardTransaction.between("2017-12-03", "2017-12-09").where(:tc_date => "2017-12-02").count)
+  end
+
+  test "lookup_customer_name works for holiday" do
+    assert_equal("TCP:Business", TimecardTransaction.find(117878).lookup_customer_name)
+  end
+
+  test "lookup_customer_name works for projects" do
+    assert_equal("TCP:Logistics:P-916", TimecardTransaction.find(177692).lookup_customer_name)
+  end
+
+  test "lookup_customer_name works for jobs" do
+    assert_equal("Facebook:Construction:1055 B1", TimecardTransaction.find(467).lookup_customer_name)
+  end
+
+  test "lookup customer name works for clients" do
+    assert_equal("LLESD", TimecardTransaction.find(164864).lookup_customer_name)
   end
 
 end
