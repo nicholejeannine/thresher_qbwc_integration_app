a54bd751916ffee935168d631b19fb384fad099c
nicholejeannine <nklerk@alopias.com>
2017-12-20 00:19:10 -0800

    Store timecard transaction id in the data field, and add each request as a separate job, so that each time card transaction has one job and can be found again using the id stored in the qbwc_jobs.data column

--- app/controllers/time_trackings_controller.rb
+++ app/controllers/time_trackings_controller.rb
@@ -14,11 +14,11 @@ class TimeTrackingsController < ApplicationController
     start_date = params[:start_date]
     end_date = params[:end_date]
     timecards = TimecardTransaction.between(start_date, end_date).all
-    request = []
     timecards.each_with_index do |t, i|
-      request[i] = t.build_request
+      request = t.build_request
+      # Process each request as a separate job, and store the TimecardTransaction PKEY in the data field. This field can be accessed by the response returned from the Web Connector, so we will use it to find the same TimeCard Transaction and change the status to "QB Stored" if it is successful
+      QBWC.add_job("AddTimeCards#{t.id}", true, '', TimeTrackingAddWorker, request, "#{t.id}")
     end
-    QBWC.add_job("AddTimeCards", true, '', TimeTrackingAddWorker, request)
     render plain: "OK"
   end
 
