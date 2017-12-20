56d5ce9c8068cc47e9a12f7a5f7be36ba57283e0
nicholejeannine <nklerk@alopias.com>
2017-12-19 17:29:29 -0800

    Add order by employee_full_name, txn_date to TimeTracking query

--- app/models/time_tracking.rb
+++ app/models/time_tracking.rb
@@ -2,6 +2,6 @@ class TimeTracking < ApplicationRecord
 
   # grab all time tracking entries between a specified start and end date
   def self.between(start_date, end_date)
-    where('`txn_date` >= ?', start_date).where('`txn_date` <= ?', end_date)
+    where('`txn_date` >= ?', start_date).where('`txn_date` <= ?', end_date).order(:employee_full_name, :txn_date)
   end
 end
