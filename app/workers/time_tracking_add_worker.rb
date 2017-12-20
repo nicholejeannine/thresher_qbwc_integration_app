0fe0a80fa742f46cd17aa12ea6cc81ca3042d43a
nicholejeannine <nklerk@alopias.com>
2017-12-20 00:52:28 -0800

    Try find transaction and change to qb_stored

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -3,6 +3,10 @@ class TimeTrackingAddWorker <  QBWC::Worker
   def handle_response(r, session, job, request, data)
     begin
     TimeTracking.parse_qb_response(r['time_tracking_ret'])
+    timecard = TimecardTransaction.find(data.to_i)
+    timecard.tc_status =  "QB Stored"
+    timecard.save
+      
     rescue StandardError => e
       QbwcError.create(:worker_class => "Time Tracking Add", :error_message => "#{e.backtrace}")
     end
