66306024c151778821ea13c2d718b4ab59f661ff
nicholejeannine <nklerk@alopias.com>
2017-12-20 00:29:15 -0800

    Undo

--- app/controllers/time_trackings_controller.rb
+++ app/controllers/time_trackings_controller.rb
@@ -17,24 +17,23 @@ class TimeTrackingsController < ApplicationController
     timecards.each_with_index do |t, i|
       request = t.build_request
       # Process each request as a separate job, and store the TimecardTransaction PKEY in the data field. This field can be accessed by the response returned from the Web Connector, so we will use it to find the same TimeCard Transaction and change the status to "QB Stored" if it is successful
-      QBWC.add_job("AddTimeCards#{t.id}", true, '', TimeTrackingAddWorker, request, "#{t.id")
+      QBWC.add_job("AddTimeCards#{t.id}", true, '', TimeTrackingAddWorker, request, "#{t.id}")
     end
-    @jobs = QBWC.jobs
-
+    render plain: "OK"
   end
 
-    # Take the start and end date and query the database to find:
-      #  For each timecard entry that falls between this start date and this end date, get me:
-      # - the transaction date
-      # - the employee list id
-      # - the customer's full name as it would appear in QB (or use list id)
-      # - the ticket number (use the ticket # to service the "item service ref" or use "Video:0100 default")
-      # - the duration (format this the funky way quickbooks wants it
+  # Take the start and end date and query the database to find:
+  #  For each timecard entry that falls between this start date and this end date, get me:
+  # - the transaction date
+  # - the employee list id
+  # - the customer's full name as it would appear in QB (or use list id)
+  # - the ticket number (use the ticket # to service the "item service ref" or use "Video:0100 default")
+  # - the duration (format this the funky way quickbooks wants it
 
-      # Pass the following params to build request: item_service_ref (or nil), txn_date, employee_list_id, customer_full_name, duration, notes (ticket_id)
+  # Pass the following params to build request: item_service_ref (or nil), txn_date, employee_list_id, customer_full_name, duration, notes (ticket_id)
 
-    # For each request, add a job to the qbwc job queue (see if Quickbooks supports bulk adds, at the moment it doens't appear to be working, but might just need to use an array instead of a hash?)
-     #
+  # For each request, add a job to the qbwc job queue (see if Quickbooks supports bulk adds, at the moment it doens't appear to be working, but might just need to use an array instead of a hash?)
+  #
 
 
 end
