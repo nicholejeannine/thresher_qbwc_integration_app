34fefe1159e5d6f25c18e9ed0b0f5a35ea9053a4
nicholejeannine <nklerk@alopias.com>
2017-12-25 06:04:01 -0800

    remove notes

--- app/controllers/time_trackings_controller.rb
+++ app/controllers/time_trackings_controller.rb
@@ -22,18 +22,5 @@ class TimeTrackingsController < ApplicationController
     render plain: "OK"
   end
 
-  # Take the start and end date and query the database to find:
-  #  For each timecard entry that falls between this start date and this end date, get me:
-  # - the transaction date
-  # - the employee list id
-  # - the customer's full name as it would appear in QB (or use list id)
-  # - the ticket number (use the ticket # to service the "item service ref" or use "Video:0100 default")
-  # - the duration (format this the funky way quickbooks wants it
-
-  # Pass the following params to build request: item_service_ref (or nil), txn_date, employee_list_id, customer_full_name, duration, notes (ticket_id)
-
-  # For each request, add a job to the qbwc job queue (see if Quickbooks supports bulk adds, at the moment it doens't appear to be working, but might just need to use an array instead of a hash?)
-  #
-
 
 end
