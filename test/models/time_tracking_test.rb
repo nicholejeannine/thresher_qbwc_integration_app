061ad049aa95e699b4828d9c6fb69e708607cfbe
nicholejeannine <nklerk@alopias.com>
2017-12-25 04:56:07 -0800

    Move TimeTrackingAddTest to separate file

--- test/models/time_tracking_test.rb
+++ test/models/time_tracking_test.rb
@@ -1,37 +1,22 @@
 require 'test_helper'
-require 'fixtures/time_tracking_hash'
 
 class TimeTrackingTest < ActiveSupport::TestCase
-  include TimeTrackingHash
 
-  test "can parse a simple hash" do
-    assert_equal(1, TimeTracking.count, "should equal 100")
+  test "between" do
+    assert_not_nil(358, TimeTracking.between("2017-12-03", "2017-12-09").count)
+    assert_equal(0, TimeTracking.between("2017-12-03", "2017-12-09").where(:txn_date => "2017-12-02").count)
   end
 
-  test "handles ref types" do
-    assert_equal("Apikelis, Roland", TimeTracking.first.employee_full_name)
-    assert_equal("Google:Construction", TimeTracking.first.customer_full_name)
-    assert_equal("Video:0100", TimeTracking.first.item_service)
-    assert_equal("PT8H0M", TimeTracking.first.duration)
-    assert_equal("Hourly Level 1", TimeTracking.first.payroll_item_wage)
-    assert_equal("V/C Upgrades", TimeTracking.first.notes)
-    assert_equal("NotBillable", TimeTracking.first.billable_status)
+  test "from_qb_duration" do
+    assert_equal(8.0, TimeTracking.first.from_qb_duration)
+    assert_equal(7.5, TimeTracking.find(260).from_qb_duration)
+    assert_equal(2.25, TimeTracking.find(88).from_qb_duration)
   end
 
-  test "updates existing hash" do
-    hash =  {"xml_attributes"=>{}, "txn_id"=>"5CA94-1183243364", "time_created"=>"2007-06-30T15:42:44-08:00", "time_modified"=>"2007-06-30T15:42:44-08:00", "edit_sequence"=>"1183243364", "txn_number"=>0, "txn_date"=>"2017-06-11", "entity_ref"=>{"xml_attributes"=>{}, "list_id"=>"5150000-1173314561", "full_name"=>"Joe Bean"}, "customer_ref"=>{"xml_attributes"=>{}, "list_id"=>"49B0000-1133993641", "full_name"=>"Google:Construction:Rooftop:P-11"}, "item_service_ref"=>{"xml_attributes"=>{}, "list_id"=>"31A0000-989981084", "full_name"=>"Video:0100"}, "duration"=>"PT7H30M", "class_ref"=>{"xml_attributes"=>{}, "list_id"=>"200000-991719211", "full_name"=>"Video:Cabling Mgmt"}, "payroll_item_wage_ref"=>{"xml_attributes"=>{}, "list_id"=>"E0000-875338868", "full_name"=>"Hourly Level 1"}, "notes"=>"Ticket 10", "billable_status"=>"NotBillable"}
-    TimeTracking.parse_qb_response(hash)
-    assert_equal(1, TimeTracking.count)
-    assert_equal("2017-06-11T00:00:00-07:00", TimeTracking.first.txn_date.iso8601)
-    assert_equal("Joe Bean", TimeTracking.first.employee_full_name)
-    assert_equal("Google:Construction:Rooftop:P-11", TimeTracking.first.customer_full_name)
-    assert_equal("PT7H30M", TimeTracking.first.duration)
-    assert_equal("Ticket 10", TimeTracking.first.notes)
-  end
-
-  def setup
-    TimeTracking.destroy_all
-    TimeTracking.parse_qb_response(qb_hash)
+  test "tc_date" do
+    assert_equal("2013-01-21", TimeTracking.first.tc_date)
+    assert_equal("2017-01-10", TimeTracking.find(260).tc_date)
+    assert_equal("2017-01-23", TimeTracking.find(1500).tc_date)
   end
 
 end
