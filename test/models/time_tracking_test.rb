291cf6056312b62f8ca7bf6d2774c9fbdaa0b1ca
nicholejeannine <nklerk@alopias.com>
2017-12-25 05:42:42 -0800

    Add TimeTracking#from_qb_duration and #tc_date for front-end API

--- test/models/time_tracking_test.rb
+++ test/models/time_tracking_test.rb
@@ -1,22 +1,22 @@
 require 'test_helper'
-
+ENV['RAILS_ENV'] = 'development'
 class TimeTrackingTest < ActiveSupport::TestCase
 
   test "between" do
-    assert_not_nil(358, TimeTracking.between("2017-12-03", "2017-12-09").count)
+    assert_not_equal(0, TimeTracking.between("2017-11-03", "2017-12-09").count)
     assert_equal(0, TimeTracking.between("2017-12-03", "2017-12-09").where(:txn_date => "2017-12-02").count)
   end
 
-  test "from_qb_duration" do
+  test "from_qb_duration - run in console" do
     assert_equal(8.0, TimeTracking.first.from_qb_duration)
     assert_equal(7.5, TimeTracking.find(260).from_qb_duration)
     assert_equal(2.25, TimeTracking.find(88).from_qb_duration)
   end
 
-  test "tc_date" do
+  test "tc_date - run in console" do
     assert_equal("2013-01-21", TimeTracking.first.tc_date)
-    assert_equal("2017-01-10", TimeTracking.find(260).tc_date)
-    assert_equal("2017-01-23", TimeTracking.find(1500).tc_date)
+    assert_equal("2016-12-28", TimeTracking.find(260).tc_date)
+    assert_equal("2017-01-18", TimeTracking.find(1500).tc_date)
   end
 
 end
