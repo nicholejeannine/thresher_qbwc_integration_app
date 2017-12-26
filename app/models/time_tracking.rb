b4670513c6059e9f4025bbdb0c8477238f57f8d6
nicholejeannine <nklerk@alopias.com>
2017-12-26 12:54:49 -0800

    Correct field

--- app/models/time_tracking.rb
+++ app/models/time_tracking.rb
@@ -1,29 +1,22 @@
 class TimeTracking < ApplicationRecord
-  after_find :format_fields
+  after_find :format_qb_duration
+  after_find :tc_date
 
   # grab all time tracking entries between a specified start and end date
   def self.between(start_date, end_date)
     where('`txn_date` >= ?', start_date).where('`txn_date` <= ?', end_date).order(:employee_full_name, :txn_date)
   end
 
-  def format_fields
-    tc_date
-    format_qb_duration
-  end
-
-
-  private
-
   # Time tracking in Quickbooks is formatted as "PT8H0M" ... Need to convert to "8.0" for API.
   def format_qb_duration
     hours = self.duration.remove(/PT/).match(/[\d]+/).to_s || 0
-    minutes = self.duration.split("H")[1].remove("M").to_f/60.0 || 0
+    minutes = (self.duration.split("H")[1].remove("M").to_f/60.0 || 0)
     minutes = minutes.to_s.split(".")[1] || 0
     self.duration = "#{hours}.#{minutes}"
   end
 
   def tc_date
-    self.duration = txn_date.strftime('%F')
+    self.txn_date = txn_date.strftime('%F')
   end
 end
 
