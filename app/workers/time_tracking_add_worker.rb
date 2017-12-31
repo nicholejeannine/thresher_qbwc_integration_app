1f35b3a329fb9da3af058bb65f2bbf3a9a5c39a3
nicholejeannine <nklerk@alopias.com>
2017-12-31 01:28:08 -0800

    Try catch naming errors

--- app/workers/time_tracking_add_worker.rb
+++ app/workers/time_tracking_add_worker.rb
@@ -1,6 +1,7 @@
 class TimeTrackingAddWorker <  QBWC::Worker
 
   def handle_response(r, session, job, request, data)
+    if r['xml_attributes']['statusCode'] == '0'
     begin
     TimeTracking.parse_qb_response(r['time_tracking_ret'])
     timecard = TimecardTransaction.find(data.to_i)
@@ -9,6 +10,9 @@ class TimeTrackingAddWorker <  QBWC::Worker
     QBWC.delete_job(job)
     rescue StandardError => e
       QbwcError.create(:worker_class => "TimeTrackingAddWorker", :model_id => data, :error_message => "#{e}")
+    end
+    else
+      QbwcError.create(:worker_class => "TimeTrackingAddWorker", :model_id => data, :error_message => "#{r['xml_attributes']['statusMessage']}")
     end
   end
 end
