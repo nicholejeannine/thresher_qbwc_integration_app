ca84692d46b32678fdbcdebb6145b32045fbcd6c
nicholejeannine <nklerk@alopias.com>
2017-12-30 18:23:52 -0800

    Try separate timecard transaction and time trackings routes

---
+++ app/controllers/timecard_transactions_controller.rb
@@ -0,0 +1,16 @@
+class TimeTrackingsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  # get '/time_trackings'
+  def index
+    if params[:start_date] && params[:end_date]
+      start_date = params[:start_date]
+      end_date = params[:end_date]
+      @entries = TimeTracking.between(start_date, end_date).all
+    else
+      @entries = TimeTracking.all
+    end
+    render json: @entries
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Quickbooks stuff
   get 'qbwc/action' => 'qbwc#_generate_wsdl'
   get 'qbwc/qwc' => 'qbwc#qwc'
+  get 'time_card_transactions' => 'timecard_transactions#index'
   get 'time_trackings' => 'time_trackings#index'
   post 'time_trackings' => 'time_trackings#add_request'
   wash_out :qbwc
