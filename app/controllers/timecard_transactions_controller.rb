d667d92bc513f58db74b0ea57b68e528927feb6f
nicholejeannine <nklerk@alopias.com>
2017-12-30 18:25:32 -0800

    Rename

--- app/controllers/timecard_transactions_controller.rb
+++ app/controllers/timecard_transactions_controller.rb
@@ -1,4 +1,4 @@
-class TimeTrackingsController < ApplicationController
+class TimecardTransactionsController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   # get '/time_trackings'
