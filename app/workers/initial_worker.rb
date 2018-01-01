a19dd440b586e8b7c8943d3535bb24b3f3cc3a33
nicholejeannine <nklerk@alopias.com>
2017-12-31 17:37:16 -0800

    Date fail? real specific on production server?

--- app/workers/initial_worker.rb
+++ app/workers/initial_worker.rb
@@ -58,7 +58,7 @@ class InitialWorker < QBWC::Worker
 			 :xml_attributes => { :requestID =>1, :iterator  => "Start" },
 			 :max_returned => 100,
 			 :modified_date_range_filter => {
-					 :from_modified_date => Date.today.months_ago(3).strftime '%FT%R'
+					 :from_modified_date => Date.today.months_ago(3).strftime('%FT%R')
 			 }
 	 }
 	 }]
