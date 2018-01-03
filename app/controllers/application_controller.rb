debc1b63cd0faa5bcd2b02008b666fd7852b7a7d
nicholejeannine <nklerk@alopias.com>
2018-01-03 14:16:14 -0800

    Test and notes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+  before_action :auth
+
+  def auth
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || ""
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,11 @@ Rails.application.routes.draw do
   get 'qbwc/action' => 'qbwc#_generate_wsdl'
   get 'qbwc/qwc' => 'qbwc#qwc'
   wash_out :qbwc
+  
+  # View all the timecard-related jobs
   get 'qbwc_jobs' => 'qbwc_jobs#index'
+  
+  # Page to view previous exports (when button pressed, a link to "successes", a link to "errors")
   resources :timecard_transactions, only: [:new, :create]
   get 'time_trackings' => 'time_trackings#index'
 
