124f69738039f31421d7e2886712dd6eede2feab
nicholejeannine <nklerk@alopias.com>
2018-01-04 12:06:48 -0800

    Reverts

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  # before_action :auth
+  before_action :auth
 
-  # def auth
-  #   response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || ""
-  #   response.headers['Access-Control-Allow-Credentials'] = 'true'
-  # end
+  def auth
+    response.headers['Access-Control-Allow-Origin'] = request.headers['Origin'] || ""
+    response.headers['Access-Control-Allow-Credentials'] = 'true'
+  end
 end
