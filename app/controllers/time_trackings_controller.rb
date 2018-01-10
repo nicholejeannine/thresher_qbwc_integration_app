52650218c4a30e63ce4e8ca0b366425642055dc3
nicholejeannine <nklerk@alopias.com>
2018-01-10 01:00:07 -0800

    The try/catch maybe broke it? Reconfigured stuff to actually work again but those aren't in these commits :)

--- app/controllers/time_trackings_controller.rb
+++ app/controllers/time_trackings_controller.rb
@@ -24,7 +24,6 @@ class TimeTrackingsController < ApplicationController
     end_date = params[:end_date]
     timecards = TimecardTransaction.between(start_date, end_date).locked
     timecards.each_with_index do |t, i|
-      begin
         if t.valid_customer?
           request = t.build_request
           # Process each request as a separate job, and store the TimecardTransaction PKEY in the data field. This field can be accessed by the response returned from the Web Connector, so we will use it to find the same TimeCard Transaction and change the status to "QB Stored" if it is successful
@@ -32,10 +31,7 @@ class TimeTrackingsController < ApplicationController
         else
           QbwcTimecardError.create(:worker_class => "TimeCardTransaction Add Request", :model_id => t.id, :error_message => "No Quickbooks customer found for request TimecardTrans #{t.id}, project_id #{t.project_id}, job_id #{t.job_id}, customer #{t.client_id}, employee #{t.employee.employee_id}, date #{t.tc_date}")
         end
-      rescue StandardError => e
-        QbwcError.create(:worker_class => self.class.name, :id => t.id, :error_message => e)
-      end
-    end
+       end
       render plain: "OK"
     end
 
