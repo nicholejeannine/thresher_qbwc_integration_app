89f0fca4169bbdabd1ffb7d9c4b60ce857db00bc
nicholejeannine <nklerk@alopias.com>
2018-01-10 00:33:35 -0800

    Add actual error block and try better errors in time card errors table

--- app/controllers/time_trackings_controller.rb
+++ app/controllers/time_trackings_controller.rb
@@ -24,15 +24,19 @@ class TimeTrackingsController < ApplicationController
     end_date = params[:end_date]
     timecards = TimecardTransaction.between(start_date, end_date).locked
     timecards.each_with_index do |t, i|
-      if t.valid_customer?
-        request = t.build_request
-        # Process each request as a separate job, and store the TimecardTransaction PKEY in the data field. This field can be accessed by the response returned from the Web Connector, so we will use it to find the same TimeCard Transaction and change the status to "QB Stored" if it is successful
-        QBWC.add_job("AddTimeCards#{t.id}", true, '', TimeTrackingAddWorker, request, t.id)
-      else
-        QbwcTimecardError.create(:worker_class => "TimeCardTransaction Add Reqeust", :model_id => t.id, :error_message => "No Quickbooks customer found for request #{i}t, Timecard transaction with id #{t.id}")
+      begin
+        if t.valid_customer?
+          request = t.build_request
+          # Process each request as a separate job, and store the TimecardTransaction PKEY in the data field. This field can be accessed by the response returned from the Web Connector, so we will use it to find the same TimeCard Transaction and change the status to "QB Stored" if it is successful
+          QBWC.add_job("AddTimeCards#{t.id}", true, '', TimeTrackingAddWorker, request, t.id)
+        else
+          QbwcTimecardError.create(:worker_class => "TimeCardTransaction Add Request", :model_id => t.id, :error_message => "No Quickbooks customer found for request TimecardTrans #{t.id}, project_id #{t.project_id}, job_id #{t.job_id}, customer #{t.client_id}, employee #{t.employee.employee_id}, date #{t.tc_date}")
+        end
+      rescue StandardError => e
+        QbwcError.create(:worker_class => self.class.name, :id => t.id, :error_message => e)
       end
     end
-    render plain: "OK"
-  end
+      render plain: "OK"
+    end
 
 end
