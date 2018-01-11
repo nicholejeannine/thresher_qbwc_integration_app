9a3fff720efe80aa6074f91cc28b5383c9bad60a
nicholejeannine <nklerk@alopias.com>
2018-01-11 13:36:22 -0800

    Clunky code, but forget self.

--- app/models/qb_customer.rb
+++ app/models/qb_customer.rb
@@ -10,11 +10,11 @@ class QbCustomer < ApplicationRecord
     end
   end
   
-  def parse_qb_customer_response(qb)
+  def self.parse_qb_customer_response(qb)
     begin
       c = self.find_or_create_by(:list_id => qb['list_id'])
       c.full_name = qb['full_name']
-      c.customer_type = self.customer_type
+      c.customer_type = self.customer_type(qb['full_name'])
       c.save
     rescue StandardError => e
       QbwcError.create(:worker_class => self.class.name, :error_message => e)
