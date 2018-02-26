4b5905f94128ab0085d1c7c2f6df41d663db8208
nicholejeannine <nklerk@alopias.com>
2018-02-26 14:44:04 -0800

    re-add last ran to employee query worker, comment last ran to just look at data for a whole customer query. test

--- app/workers/query_worker.rb
+++ app/workers/query_worker.rb
@@ -5,15 +5,14 @@ class QueryWorker < QBWC::Worker
 
   def requests(job, session, data)
    [
-    # {:customer_query_rq => {
-    #    :xml_attributes => { :requestID =>1, :iterator  => "Start" },
-    #    :max_returned => 100,
-    #    :active_status => "All",
-    #    # :from_modified_date => last_ran,
-    #    :include_ret_element => ['ListID', 'TimeCreated', 'TimeModified', 'EditSequence', 'Name', 'FullName', 'IsActive', 'ParentRef', 'Sublevel', 'CompanyName', 'Salutation', 'FirstName', 'MiddleName', 'LastName', 'JobTitle', 'BillAddress', 'ShipAddress','Phone', 'AltPhone', 'Fax', 'Email', 'Cc', 'Contact', 'AltContact', 'CustomerTypeRef', 'TermsRef', 'SalesRepRef', 'Balance', 'TotalBalance', 'SalesTaxCodeRef', 'ItemSalesTaxRef', 'AccountNumber', 'JobStatus', 'JobStartDate', 'JobProjectedEndDate', 'JobEndDate', 'JobDesc', 'JobTypeRef', 'PreferredDeliveryMethod', 'DataExtRet'],
-    #     :owner_id => 0
-    #    }
-    #  },
+    {:customer_query_rq => {
+       :xml_attributes => { :requestID =>1, :iterator  => "Start" },
+       :max_returned => 100,
+       :active_status => "All",
+       # :from_modified_date => last_ran,
+        :owner_id => 0
+       }
+     },
      {:vendor_query_rq => {
             :xml_attributes => { :requestID =>1, :iterator  => "Start" },
             :max_returned => 100,
@@ -72,7 +71,7 @@ class QueryWorker < QBWC::Worker
         },
         {:employee_query_rq => {
                     :active_status => "All",
-                    # :from_modified_date => last_ran,
+                    :from_modified_date => last_ran,
                     :include_ret_element => ['ListID', 'Name', 'IsActive', 'FirstName', 'MiddleName', 'LastName']
                 }
                 # },
@@ -90,7 +89,7 @@ class QueryWorker < QBWC::Worker
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
    begin
-      r['customer_ret']&.each{|qb|Customer.parse_customer_response(qb)}
+      # r['customer_ret']&.each{|qb|Customer.parse_customer_response(qb)}
       r['vendor_ret']&.each{|qb|Vendor.parse_qb_response(qb)}
       r['estimate_ret']&.each{|qb|Estimate.parse_qb_response(qb)}
       r['sales_order_ret']&.each{|qb|SalesOrder.parse_qb_response(qb)}
