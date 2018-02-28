a4970cfedd15065c3f3b2dbe3f038426b1512cea
nicholejeannine <nklerk@alopias.com>
2018-02-27 21:03:00 -0800

    Use simple hash lookup for the simple lookup values for client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,6 +1,34 @@
 class Client < ApplicationRecord
   FIELDS = %w(Cust_Company Cust_NameSalutation Cust_NameFirst Cust_NameMiddle Cust_NameLast Cust_PhoneOffice Cust_EmailTo Cust_PhoneAlt Cust_EmailCC Cust_PhoneCell Cust_PhoneFax Cust_BillTo_Company Cust_BillTo_Name Cust_BillTo_Address1 Cust_BillTo_Address2 Cust_BillTo_City Cust_BillTo_State Cust_BillTo_Zip Cust_ShipTo_Company Cust_ShipTo_Name Cust_ShipTo_Address1 Cust_ShipTo_Address2 Cust_ShipTo_City Cust_ShipTo_State Cust_ShipTo_Zip Cust_InactiveFlag site_contact site_email site_phone sales_rep)
   
+  FIELD_MAP = {
+      :Cust_Company => 'company_name',
+      :Cust_NameSalutation => 'salutation',
+      :Cust_NameFirst => 'first_name',
+      :Cust_NameMiddle => 'middle_name',
+      :Cust_NameLast => 'last_name',
+      :Cust_PhoneOffice => 'phone',
+      :Cust_EmailTo => 'email',
+      :Cust_PhoneAlt => 'alt_phone',
+      :Cust_EmailCC => 'cc',
+      :Cust_PhoneFax => 'fax',
+      # :Cust_BillTo_Company => {:bill_address => "addr1"},
+      # :Cust_BillTo_Name => {:bill_address => "addr2"},
+      # :Cust_BillTo_Address1 => {:bill_address => "addr3"},
+      # :Cust_BillTo_Address2 => {:bill_address => "addr4"},
+      # :Cust_BillTo_City => {:bill_address => "city"},
+      # :Cust_BillTo_State => {:bill_address => "state"},
+      # :Cust_BillTo_Zip => {:bill_address => "postal_code"},
+      # :Cust_ShipTo_Company => {:ship_address => "addr1"},
+      # :Cust_ShipTo_Name => {:ship_address => "addr2"},
+      # :Cust_ShipTo_Address1 => {:ship_address => "addr3"},
+      # :Cust_ShipTo_Address2 => {:ship_address => "addr4"},
+      # :Cust_ShipTo_City => {:ship_address => "city"},
+      # :Cust_ShipTo_State => {:ship_address => "state"},
+      # :Cust_ShipTo_Zip => {:ship_address => "postal_code"},
+      # :sales_rep => {:sales_rep_ref => "full_name"}
+  }
+  
   self.table_name = "Customers"
   self.primary_key = "Customers_PKEY"
   
@@ -15,22 +43,25 @@ class Client < ApplicationRecord
         # customer.Cust_CompanyAbr = qb['full_name']
       else # SET ALL FIELDS TO DEFAULT VALUES
         Client::FIELDS.each {|x| customer[x] = ""}
-        customer.Cust_Company = qb['company_name']
-        customer.Cust_NameSalutation = qb['salutation']
-        customer.Cust_NameFirst = qb['first_name']
-        customer.Cust_NameMiddle = qb['middle_name']
-        customer.Cust_NameLast = qb['last_name']
-        customer.Cust_PhoneOffice = qb['phone']
-        customer.Cust_EmailTo = qb['email']
-        customer.Cust_PhoneAlt = qb['alt_phone']
-        customer.Cust_EmailCC = qb['cc']
+        Client::FIELD_MAP.each do |key, value|
+          customer[key] = qb[value]
+        end
+        # customer.Cust_Company = qb['company_name']
+        # customer.Cust_NameSalutation = qb['salutation']
+        # customer.Cust_NameFirst = qb['first_name']
+        # customer.Cust_NameMiddle = qb['middle_name']
+        # customer.Cust_NameLast = qb['last_name']
+        # customer.Cust_PhoneOffice = qb['phone']
+        # customer.Cust_EmailTo = qb['email']
+        # customer.Cust_PhoneAlt = qb['alt_phone']
+        # customer.Cust_EmailCC = qb['cc']
         if qb.has_key?("additional_contact_ref")
           ref = qb["additional_contact_ref"]
           if ref.pluck("contact_name").include?("Mobile")
             customer.Cust_PhoneCell = ref.find_all {|e| e['contact_name'] == 'Mobile'}.pluck("contact_value")[0]
           end
         end
-        customer.Cust_PhoneFax = qb['fax']
+        # customer.Cust_PhoneFax = qb['fax']
         if qb.has_key?("bill_address")
           customer.Cust_BillTo_Company = qb["bill_address"]["addr1"]
           customer.Cust_BillTo_Name = qb['bill_address']['addr2']
