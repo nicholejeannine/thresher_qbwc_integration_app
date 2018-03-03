9531a01cff8005ae35d07978be9a908a6606f71c
nicholejeannine <nklerk@alopias.com>
2018-03-02 21:45:18 -0800

    Comments

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -2,6 +2,11 @@ class ApplicationRecord < ActiveRecord::Base
   include QuickbooksTypes
   self.abstract_class = true
 
+  no_field_map = Proc.new{NoMethodError.new("No FIELD_MAP Constant has been initiated by this class")}
+  # Every table that syncs data with the portal needs to define a hash constant, FIELD_MAP.  FIELD_MAP's keys are the portal columns that the web connector can update. The values are "lookup" values, e.g., they specify where and how to obtain the correct values from the Quickbooks query hash.  The value types are either a string, a hash, or a proc (a special method that can be performed on a portion of the quickbooks hash to obtain the correct value.
+  # This line of code is intented to help anyone who might come along after Nichole and wants to add a new class for the web connector to pull data from. If you haven't yet implemented a FIELD_MAP hash, you should do so.
+  FIELD_MAP = no_field_map.call
+
   def self.is_valid_key?(key)
     key.in?(self.column_names)
   end
@@ -23,4 +28,5 @@ class ApplicationRecord < ActiveRecord::Base
       QbwcError.create(:worker_class => "#{self.class.name}", :model_id => "#{qb_value}", :error_message => "Error parsing response: #{e}")
     end
   end
+  
 end
