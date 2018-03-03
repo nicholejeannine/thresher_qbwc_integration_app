b2af9cc7648a468c99d5588dc6c002f399b96080
nicholejeannine <nklerk@alopias.com>
2018-03-02 17:21:24 -0800

    Replace QuickbooksTypes with procs

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,33 +1,5 @@
 class Client < ApplicationRecord
-  
-  SITE_CONTACT_CAST = Proc.new{|data|
-    if data.pluck("data_ext_name").include?("Site Contact")
-    data.find_all {|x| x['data_ext_name'] == 'Site Contact'}.pluck("data_ext_value")[0]
-    end
-  }
-  SITE_PHONE_CAST = Proc.new{|data|
-    if data.pluck("data_ext_name").include?("Site Phone")
-      data.find_all {|x| x['data_ext_name'] == 'Site Phone'}.pluck("data_ext_value")[0]
-    end
-  }
-
-  SITE_EMAIL_CAST = Proc.new{|data|
-    if data.pluck("data_ext_name").include?("Site Email")
-      data.find_all {|x| x['data_ext_name'] == 'Site Email'}.pluck("data_ext_value")[0]
-    end
-  }
-  
-  MOBILE_CAST = Proc.new{|data|
-    if data.pluck("contact_name").include?("Mobile")
-      data.find_all {|e| e['contact_name'] == 'Mobile'}.pluck("contact_value")[0]
-    end
-  }
-
-  INACTIVE_FLAG_CAST = Proc.new{|data|
-    "X" unless data
-  }
-  
-  FIELDS = %w(Cust_Company Cust_NameSalutation Cust_NameFirst Cust_NameMiddle Cust_NameLast Cust_PhoneOffice Cust_EmailTo Cust_PhoneAlt Cust_EmailCC Cust_PhoneCell Cust_PhoneFax Cust_BillTo_Company Cust_BillTo_Name Cust_BillTo_Address1 Cust_BillTo_Address2 Cust_BillTo_City Cust_BillTo_State Cust_BillTo_Zip Cust_ShipTo_Company Cust_ShipTo_Name Cust_ShipTo_Address1 Cust_ShipTo_Address2 Cust_ShipTo_City Cust_ShipTo_State Cust_ShipTo_Zip Cust_InactiveFlag site_contact site_email site_phone sales_rep)
+  include QuickbooksTypes
   
   FIELD_MAP = {
       :list_id => "list_id",
@@ -74,7 +46,7 @@ class Client < ApplicationRecord
         customer = Client.find_or_create_by(:Cust_CompanyAbr => qb['full_name'])
       end
       # If customer if found, save the qb fields to their corresponding Thresher fields.
-      Client::FIELDS.each {|x| customer[x] = ""}
+      Client::FIELD_MAP.keys.each {|x| customer[x] = ""}
       Client::FIELD_MAP.each do |k, v|
         if v.is_a?(Hash)
           v.each do |key, value|
--- app/models/concerns/quickbooks_types.rb
+++ app/models/concerns/quickbooks_types.rb
@@ -2,22 +2,32 @@ module QuickbooksTypes
 	extend ActiveSupport::Concern
 	included do
 		
-		# Determine whether the model's unique id is called "list_id", "txn_id", or "txn_line_id"
-		def self.qb_id
-			case self.name
-			when "Client", "Job", "Project", "Vendor", "Employee", "QbEmployee", "QbCustomer"
-				"list_id"
-			when "Estimate", "Invoice", "PurchaseOrder", "ReceivePayment", "SalesOrder", "TimeTracking"
-				"txn_id"
-			when "EstimateLine", "InvoiceLine", "PurchaseOrderLine", "SalesOrderLine"
-				"txn_line_id"
+		SITE_CONTACT_CAST = Proc.new{|data|
+			if data.pluck("data_ext_name").include?("Site Contact")
+				data.find_all {|x| x['data_ext_name'] == 'Site Contact'}.pluck("data_ext_value")[0]
 			end
-		end
-
-		# These are fields Quickbooks returns that we never care about and can discard immediately.
-		def self.ignored_type?(key)
-			key.match(/addr5$|card_txn_info|prefill_account_ref|external_guid|group_ret$|^other|contact_ref$|contacts_ret$|card_info$|currency_ref$|ship_to_address$|block$|xml_attributes|notes_ret|currency$|exchange_rate|inventory_site_location_ref|^fob$|ar_account_ref$/)
-		end
+		}
+		SITE_PHONE_CAST = Proc.new{|data|
+			if data.pluck("data_ext_name").include?("Site Phone")
+				data.find_all {|x| x['data_ext_name'] == 'Site Phone'}.pluck("data_ext_value")[0]
+			end
+		}
+		
+		SITE_EMAIL_CAST = Proc.new{|data|
+			if data.pluck("data_ext_name").include?("Site Email")
+				data.find_all {|x| x['data_ext_name'] == 'Site Email'}.pluck("data_ext_value")[0]
+			end
+		}
+		
+		MOBILE_CAST = Proc.new{|data|
+			if data.pluck("contact_name").include?("Mobile")
+				data.find_all {|e| e['contact_name'] == 'Mobile'}.pluck("contact_value")[0]
+			end
+		}
+		
+		INACTIVE_FLAG_CAST = Proc.new{|data|
+			"X" unless data
+		}
 
 	end
 
