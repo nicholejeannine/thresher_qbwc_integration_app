ab78c2b76d5fda2955f6635be5b2ecc8b2fd2bec
nicholejeannine <nklerk@alopias.com>
2018-03-06 17:07:50 -0800

    Add initial job worker

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,5 @@ QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
 QBWC.add_job(:list_customers, false, '', CustomerQueryWorker)
 
 QBWC.add_job(:initalize_customers, true, '', Initializers::ClientWorker)
+QBWC.add_job(:initalize_jobs, true, '', Initializers::JobWorker)
 
