3f5b94cfe1fe10e74cffd359f2e799c68ca5a270
nicholejeannine <nklerk@alopias.com>
2018-03-09 13:41:33 -0800

    Reseed database. Ready to merge into master - will sync clients to Thresher Customers table

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,10 @@
 # clear quickbooks jobs
 QBWC.clear_jobs
-QBWC.add_job(:initial, false, '', InitialWorker)
-QBWC.add_job(:query, false, '', QueryWorker)
-QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
-QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
-QBWC.add_job(:list_customers, false, '', CustomerQueryWorker)
 
 QBWC.add_job(:initalize_customers, true, '', Initializers::ClientWorker)
-QBWC.add_job(:initalize_jobs, true, '', Initializers::JobWorker)
+QBWC.add_job(:query, true, '', QueryWorker)
+QBWC.add_job(:list_customers, true, '', CustomerQueryWorker)
+QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
+QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
+# QBWC.add_job(:initalize_jobs, true, '', Initializers::JobWorker)
 
