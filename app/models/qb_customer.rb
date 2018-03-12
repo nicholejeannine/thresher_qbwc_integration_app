2109332662dd807940cd02fe2c1e0051428168be
nicholejeannine <nklerk@alopias.com>
2018-03-12 15:08:09 -0700

    Remove QbCustomer dependence on QuickbooksQueryResponse

--- app/models/qb_customer.rb
+++ app/models/qb_customer.rb
@@ -1,21 +1,11 @@
 class QbCustomer < ApplicationRecord
   
   
-  def self.customer_type(key)
-    if key.match(/P-\d+$/)
-      "Project"
-    elsif key.match(/:/)
-      "Job"
-    else "Client"
-    end
-  end
-  
   def self.parse_qb_customer_response(qb)
     begin
-      qb_value = qb[self.qb_id]
-      c = self.find_or_create_by(self.qb_id => qb_value)
-      hash = QuickbooksQueryResponse.parse(qb, self, c.id)
-      c.update(hash)
+      c = self.find_or_initialize_by("list_id" => qb['list_id'])
+      c['full_name'] = qb['full_name']
+      c.save
     rescue StandardError => e
       QbwcError.create(:worker_class => self.class.name, :error_message => e)
     end
