7c0fabfabbef3df8f8fbc87fb2c837d9c45871c3
nicholejeannine <nklerk@alopias.com>
2018-03-19 11:10:25 -0700

    Add route for add project via CustomerAddRq

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,16 @@
+class ProjectsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  layout false
+
+  def create
+    # Take in the parent full name from params
+    # Add a new project - get the id
+    # Build a new customer add request, using the id to create a new customer (project)
+    # Add the QBWC job
+    # Render plain "okay"
+    parent_full_name = params[:full_name]
+    request = Project.customer_add_request(parent_full_name)
+    QBWC.add_job("AddProject", true, '', request)
+    render plain: "OK"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,10 @@ Rails.application.routes.draw do
   # Time trackings are successfully exported time card transactions - we can just view those
   resources :time_trackings, only: [:index, :create]
   
+  # Post request to '/projects' will push a new customer (project) to QB
+  post '/projects' => 'projects#create'
+  
+  # These are just views documenting the sync process:
   get '/projects' => 'home#projects'
   get '/customers' => 'home#customers'
   get '/jobs' => 'home#jobs'
