cd531b99ed94cbdaa0945cc1ecac4de56cef01c2
nicholejeannine <nklerk@alopias.com>
2018-03-19 13:28:04 -0700

    Add FIELD_MAP constant to Project for qb sync

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,24 +1,40 @@
 class Project < ApplicationRecord
-  before_save :titleize_job_status
 
-  # FIXME: THIS MUST LOOK AT A THE VIEW OF STUFF IN THE PORTAL
-  def self.parse_qb_response(qb)
-    begin
-      # First try to find by list id
-      p = Project.find_by("list_id" => qb['list_id']) || Project.find_by("full_name" => qb["full_name"])
-      if p.nil?
-        raise StandardError, "Project Not Found"
-      else
-        hash = QuickbooksQueryResponse.parse(qb, self, p.id)
-        p.update(hash)
-      end
-    rescue StandardError => e
-      QbwcError.create(:worker_class => "Project", :model_id => "#{qb["full_name"]}", :error_message => "#{e}")
-    end
-  end
-  
-  # job_status fields come back like "InProgress" - make it save as two separate words.
-  def titleize_job_status
-    self.job_status = self.job_status&.titleize
-  end
+  self.table_name = "Project"
+  self.primary_key = "Project_PKEY"
+
+  FIELD_MAP = {
+      :list_id => "list_id",
+      :full_name => 'full_name',
+      :Project_DateStartActual => 'job_start_date',
+      :Project_DateEndEstimated => 'job_projected_end_date',
+      :Project_DateEndActual => 'job_end_date',
+      :Project_NameSalutation => 'salutation',
+      :Project_NameFirst => 'first_name',
+      :Project_NameMiddle => 'middle_name',
+      :Project_NameLast => 'last_name',
+      :Project_PhoneMain => 'phone',
+      :Project_EmailMain => 'email',
+      :Project_PhoneAlternate => 'alt_phone',
+      :Project_EmailCC => 'cc',
+      :Project_PhoneCell => {"additional_contact_ref" => MOBILE_CAST},
+      :Project_PhoneFax => 'fax',
+      :bill_addr1 => {"bill_address" => "addr1"},
+      :bill_addr2 => {"bill_address" => "addr2"},
+      :bill_addr3 => {"bill_address" => "addr3"},
+      :bill_addr4 => {"bill_address" => "addr4"},
+      :bill_city => {"bill_address" => "city"},
+      :bill_state => {"bill_address" => "state"},
+      :bill_postal_code => {"bill_address" => "postal_code"},
+      :Project_Address1 => {"ship_address" => "addr1"},
+      :Project_Address2 => {"ship_address" => "addr2"},
+      :ship_addr3 => {"ship_address" => "addr3"},
+      :ship_addr4 => {"ship_address" => "addr4"},
+      :Project_City => {"ship_address" => "city"},
+      :Project_State => {"ship_address" => "state"},
+      :Project_Zip => {"ship_address" => "postal_code"},
+      :is_active => "is_active",
+      :sales_rep => {"sales_rep_ref" => "full_name"}
+  }
+
 end
