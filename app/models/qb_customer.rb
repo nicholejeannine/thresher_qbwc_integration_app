09c58a65ad3f1a2404e8960f84bf80e86db70410
nicholejeannine <nklerk@alopias.com>
2018-03-26 10:39:35 -0700

    Modify customer query worker to add name field in order to check for duplicate entries in QB with same PKEY values

--- app/models/qb_customer.rb
+++ app/models/qb_customer.rb
@@ -4,6 +4,7 @@ class QbCustomer < ApplicationRecord
     begin
       c = self.find_or_initialize_by("list_id" => qb['list_id'])
       c['full_name'] = qb['full_name']
+      c['name'] = qb['name']
       c.save
     rescue StandardError => e
       QbwcError.create(:worker_class => self.class.name, :error_message => e)
