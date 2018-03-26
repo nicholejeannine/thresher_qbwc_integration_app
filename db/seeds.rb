f97c91ef18a40a4e5fc89c30ea834c6280a2325a
nicholejeannine <nklerk@alopias.com>
2018-03-26 09:34:03 -0700

    Reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,11 @@
 QBWC.clear_jobs
 # THE INITIALIZER JOBS DELETE THEMSELVES AFTER THEY ARE RUN. If you need to run them again for some reason, uncomment them.
 
-# QBWC.add_job(:initalize_customers, true, '', Initializers::ClientWorker)
-# QBWC.add_job(:initalize_jobs, true, '', Initializers::JobWorker)
-QBWC.add_job(:initialize_projects, true, '', Initializers::ProjectWorker)
-QBWC.add_job(:query, true, '', QueryWorker)
+QBWC.add_job(:initalize_customers, true, '', Initializers::ClientWorker)
+QBWC.add_job(:initalize_jobs, true, '', Initializers::JobWorker)
+# QBWC.add_job(:initialize_projects, true, '', Initializers::ProjectWorker)
+# QBWC.add_job(:query, true, '', QueryWorker)
 QBWC.add_job(:list_customers, true, '', CustomerQueryWorker)
-QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
-QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
+# QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
+# QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
 
