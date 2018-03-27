4bc9588088ade4ac89a367977d7c009d41d7b85a
nicholejeannine <nklerk@alopias.com>
2018-03-26 19:21:16 -0700

    Modify query worker to just pull employees, reseed to pull employee ids

--- app/workers/query_worker.rb
+++ app/workers/query_worker.rb
@@ -72,7 +72,7 @@ class QueryWorker < QBWC::Worker
      #    },
         {:employee_query_rq => {
                     :active_status => "All",
-                    :from_modified_date => last_ran,
+                    # :from_modified_date => last_ran,
                     :include_ret_element => ['ListID', 'Name', 'IsActive', 'FirstName', 'MiddleName', 'LastName']
                 }
                 # },
@@ -90,7 +90,7 @@ class QueryWorker < QBWC::Worker
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
    begin
-      r['customer_ret']&.each{|qb|Customer.parse_customer_response(qb)}
+      # r['customer_ret']&.each{|qb|Customer.parse_customer_response(qb)}
       # r['vendor_ret']&.each{|qb|Vendor.parse_qb_response(qb)}
       # r['estimate_ret']&.each{|qb|Estimate.parse_qb_response(qb)}
       # r['sales_order_ret']&.each{|qb|SalesOrder.parse_qb_response(qb)}
