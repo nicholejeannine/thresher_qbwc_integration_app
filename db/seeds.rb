ea31e9c2e55913f38272e384c4b22e9c8738f9b4
nicholejeannine <nklerk@alopias.com>
2018-03-26 20:48:58 -0700

    Reseed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,8 @@ QBWC.clear_jobs
 # QBWC.add_job(:initalize_customers, true, '', Initializers::ClientWorker)
 # QBWC.add_job(:initalize_jobs, true, '', Initializers::JobWorker)
 # QBWC.add_job(:initialize_projects, true, '', Initializers::ProjectWorker)
-QBWC.add_job(:query, true, '', QueryWorker)
-# QBWC.add_job(:list_customers, true, '', CustomerQueryWorker)
+# QBWC.add_job(:query, true, '', QueryWorker)
+QBWC.add_job(:list_customers, true, '', CustomerQueryWorker)
 # QBWC.add_job(:list_del, false, '', ListDeletedQueryWorker)
 # QBWC.add_job(:txn_del, false, '', TxnDeletedQueryWorker)
 
