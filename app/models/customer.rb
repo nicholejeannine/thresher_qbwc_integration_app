a0f2f5a927df31e1bac31d8c2c845f27aa68519b
nicholejeannine <nklerk@alopias.com>
2018-04-09 09:53:59 -0700

    Comments

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,9 @@
-# In Quickbooks, anything that is a customer, job, or project according to Thresher is somehow still a "customer". We can only pull "customers" from Quickbooks, but we can determine by the customer name what category Thresher considers this "customer" to be.
+# In Quickbooks, anything that is a customer, job, or project according to Thresher is somehow still a "customer". We can only pull "customers" from Quickbooks, but we can determine by the customer name what category Thresher considers this "customer" to be (Client, Job, Project).
 class Customer < ApplicationRecord
 	self.abstract_class = true
 	include QbCanPushToPortal
 
-  # Determines whether the Quickbooks Customer is a Thresher client, job, or project
+  # Determines whether the Quickbooks Customer corresonds to  Thresher client, job, or project
 	def self.customer_type(full_name)
 		return Project if full_name.upcase.match(/P-\d+$/)
 		return Job if full_name.match(/:/)
