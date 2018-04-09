809b728e13aaed8ca97d3d557dd5d9969903db3f
nicholejeannine <nklerk@alopias.com>
2018-04-09 16:24:23 -0700

    Switch to Thresher db for project_full_names

--- app/models/project_full_name.rb
+++ app/models/project_full_name.rb
@@ -1,5 +1,5 @@
 class ProjectFullName < ActiveRecord::Base
   self.primary_key= :id
-  self.table_name = 'qb.project_full_names'
+  self.table_name = 'project_full_names'
   belongs_to :project, optional: true
 end
