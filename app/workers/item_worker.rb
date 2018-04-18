48909fe0fd0ba49002a46a4963de75649be963bd
nicholejeannine <nklerk@alopias.com>
2018-04-18 16:11:52 -0700

    Add simple model for items, inline the response, inventory items only to start

--- app/workers/item_worker.rb
+++ app/workers/item_worker.rb
@@ -9,15 +9,35 @@ class ItemWorker < QBWC::Worker
     }
     }
   end
-  
+  # Let's just handle inventory and non-inventory items, and inline it since this is a one-off request.
   def handle_response(r, session, job, request, data)
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
     begin
-      r['item_non_inventory_ret']&.each{|qb|Rails.logger.warn(qb)}
+      r['item_non_inventory_ret']&.each do |qb|
+        item = Item.new
+        item[:name] = qb["name"]
+        item[:full_name] = qb["full_name"]
+        item[:sales_desc] = qb["sales_desc"]
+        item[:sales_price] = qb["sales_price"]
+        item[:purchase_desc] = qb["purchase_desc"]
+        item[:purchase_cost] = qb["purchase_cost"]
+        item[:is_invetory] = 1
+        item.save
+      end
       # r['item_inventory_ret']&.each{|qb|Rails.logger.warn(qb)}
     rescue StandardError => e
       QbwcError.create(:worker_class => self.class.name, :error_message => e)
     end
   end
 end
+
+
+#
+# Inventory:
+#       :name => "name",
+# #     :full_name => "full_name",
+# #     :sales_desc => "sales_desc",
+# #     :sales_price => "sales_price",
+# #     :purchase_desc => "purchase_desc",
+# #     :purchase_cost => "purchase_cost"
