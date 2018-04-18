309b039d4a858b022b2c6a30d214e71480c25000
nicholejeannine <nklerk@alopias.com>
2018-04-18 15:08:56 -0700

    Reseed, and first attempt to query items.

--- app/workers/item_worker.rb
+++ app/workers/item_worker.rb
@@ -1,8 +1,12 @@
 class ItemWorker < QBWC::Worker
   
   def requests(job, session, data)
-    {
-    
+    {:item_query_rq => {
+        :xml_attributes => {:iterator  => "Start" },
+        :max_returned => 100,
+        :active_status => "All",
+        # :include_ret_element => ['ListID', 'Name', 'FullName', 'BarCodeValue', 'IsActive'],
+    }
     }
   end
   
@@ -10,7 +14,8 @@ class ItemWorker < QBWC::Worker
     # handle_response will get customers in groups of 100. When this is 0, we're done.
     complete = r['xml_attributes']['iteratorRemainingCount'] == '0'
     begin
-      
+      r['item_non_inventory_ret']&.each{|qb|Rails.logger.warn(qb)}
+      r['item_inventory_ret']&.each{|qb|Rails.logger.warn(qb)}
     rescue StandardError => e
       QbwcError.create(:worker_class => self.class.name, :error_message => e)
     end
